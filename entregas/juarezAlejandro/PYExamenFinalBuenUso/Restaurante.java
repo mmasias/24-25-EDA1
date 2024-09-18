bcaa979c64b032e2ca9e20e46abb500f0e411f16
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-09-19 00:46:50 +0200

    Otra opcion para el buen uso
    Se realizó una pequeña modificación para poder tener en cuenta otra manera en la que también pueden existir defirentes formas de almacenarlo en memoria

--- entregas/juarezAlejandro/PYExamenFinalBuenUso/Restaurante.java
+++ entregas/juarezAlejandro/PYExamenFinalBuenUso/Restaurante.java
@@ -1,23 +1,29 @@
 class Restaurante {
 
     private void atender() {
+
         Cocinero bobEsponja = new Cocinero();
         Camarero calamardo = new Camarero();
 
-        bobEsponja.prepararHamburguesa();
-        calamardo.recibir(bobEsponja.entregar());
-
-        calamardo.servir();
-        System.out.println("-".repeat(85));
-        calamardo.servir();
-        System.out.println("-".repeat(85));
-        calamardo.servir();
-        System.out.println("-".repeat(85));
-        calamardo.servir();
-        System.out.println("-".repeat(85));
-        calamardo.servir();
-        System.out.println("-".repeat(85));
-        
+        for (int i = 0; i < 5; i++) {
+            bobEsponja.prepararHamburguesa();
+            calamardo.recibir(bobEsponja.entregar());
+            calamardo.servir();
+            System.out.println("-".repeat(85));
+        }
+        /*
+         * Tambien sirve como ejemplo
+         * calamardo.servir();
+         * System.out.println("-".repeat(85));
+         * calamardo.servir();
+         * System.out.println("-".repeat(85));
+         * calamardo.servir();
+         * System.out.println("-".repeat(85));
+         * calamardo.servir();
+         * System.out.println("-".repeat(85));
+         * calamardo.servir();
+         * System.out.println("-".repeat(85));
+         */
     }
 
     public static void main(String[] args) {
