de5ee850cb67fb5d7dbeff89f6d58695cc0ac301
kelvia.barros <barroskelvia122@gmail.com>
2024-09-19 22:51:01 +0200

    A little advance on Simulation class based on the original code

--- entregas/nevesKelvia/Simulation.java
+++ entregas/nevesKelvia/Simulation.java
@@ -2,8 +2,15 @@ package entregas.nevesKelvia;
 
 public class Simulation {
     public static void main(String[] args) {
-        Wordl wordl = new Wordl(10, 25);
-        VacuumCleaner vacuum = new VacuumCleaner(wordl);
-        wordl.contaminate();
+        World world = new World(10, 25);
+        VacuumCleaner vacuum = new VacuumCleaner(world);
+        world.contaminate();
+
+        while (world.isDirty()) {
+            vacuum.move();
+            vacuum.clean();
+            world.printWorld(vacuum.getPosition());
+            pause(0.5);
+        }
     }
 }
