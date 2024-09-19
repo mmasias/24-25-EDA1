e6a1d4ecffdd067afcb8c4438117d8ec7b36bb66
manuel@AIO.universidad <manuel@masiasweb.com>
2024-09-19 17:12:36 +0200

    Añade modelos y configuración inicial

---
+++ src/ejercicio-01/hamburguesa/Huevo.java
@@ -0,0 +1,13 @@
+
+class Huevo extends Extra {
+    final String REPRESENTACION = " ~-~-(  )~-~-";
+
+    public Huevo() {
+        super("Huevo frito");
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println(REPRESENTACION);
+    }
+}
