59aab9254e839e37cb4966c9a9e47806ec4867d8
Pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2024-09-19 17:48:08 +0200

    creacion carpeta con codigo del final

---
+++ entregas/ReyOrtizPablo/Entrega-001/Carne.java
@@ -0,0 +1,10 @@
+public abstract class Carne extends Ingrediente {
+    protected String admiteCocion;
+    public Carne(String nombre,String admiteCocion){
+        this.nombre = nombre;
+        this.admiteCocion = admiteCocion;
+    } 
+    public  String describir(){
+        return nombre + "("+admiteCocion + ")";
+    }
+} 
---
+++ entregas/ReyOrtizPablo/Entrega-001/Pan.java
@@ -0,0 +1,6 @@
+public abstract class Pan extends Ingrediente {
+    public Pan(String nombre) {
+        this.nombre = nombre;
+
+    }
+}
