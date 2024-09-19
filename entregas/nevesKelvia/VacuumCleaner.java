2b564f287254a23e4196b6a55c64125e34e9ab06
kelvia.barros <barroskelvia122@gmail.com>
2024-09-19 23:08:08 +0200

    Added functions to the VacuumCleaner class for it to move, clean and get positioned. Based on the orginal code as well

--- entregas/nevesKelvia/VacuumCleaner.java
+++ entregas/nevesKelvia/VacuumCleaner.java
@@ -1,5 +1,37 @@
 package entregas.nevesKelvia;
 
 public class VacuumCleaner {
-    
+    private int[] position;
+    private World world;
+
+    public VacuumCleaner(World world) {
+        this.world = world;
+        this.position = new int[]{0, 0};
+    }
+
+    public void move() {
+        int[] previousPosition = position.clone();
+        int[][] directions = {
+                { -1, 1 }, { 0, 1 }, { 1, 1 },
+                { -1, 0 }, { 0, 0 }, { 1, 0 },
+                { -1, -1 }, { 0, -1 }, { 1, -1 }
+        };
+
+        int[] movement = directions[(int) (Math.random() * directions.length)];
+        position[0] = position[0] + movement[0];
+        position[1] = position[1] + movement[1];
+
+        if (!world.isValidPosition(position)) {
+            position = previousPosition;
+        }
+    }
+
+    public void clean() {
+        world.cleanTile(position);
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
 }
+
