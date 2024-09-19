226a5f6add0ae2073b73804dbbe0874664561bc8
Alejandro Viñas Portilla <114479197+AlejandroVinas@users.noreply.github.com>
2024-09-19 20:53:54 +0200

    Add files via upload

---
+++ entregas/Vinas_Alejandro/Reto_1/src/CocineroMal.java
@@ -0,0 +1,40 @@
+class CocineroMal extends Empleado {
+
+    public void prepararHamburguesa() {
+        final int NUMERO_EXTRAS = 4;
+        Carne laCarne = new Res("poco hecha");
+        hamburguesa = new Hamburguesa(new Brioche(), new Brioche(), laCarne, NUMERO_EXTRAS);
+        hamburguesa = new Hamburguesa(new Brioche(), new Brioche(), laCarne, NUMERO_EXTRAS);
+        hamburguesa = new Hamburguesa(new Integral(), new Integral(), laCarne, NUMERO_EXTRAS);
+        hamburguesa = new Hamburguesa(new Integral(), new Brioche(), laCarne, NUMERO_EXTRAS);
+
+        hamburguesa.agregarExtra(new Cheddar());
+        hamburguesa.agregarExtra(new Ketchup("poco"));
+        hamburguesa.agregarExtra(new Mayonesa("mucho"));
+        hamburguesa.agregarExtra(new Huevo());
+
+        hamburguesa.agregarExtra(new Cheddar());
+        hamburguesa.agregarExtra(new Ketchup("poco"));
+        hamburguesa.agregarExtra(new Mayonesa("mucho"));
+        hamburguesa.agregarExtra(new Huevo());
+
+        hamburguesa.agregarExtra(new Cheddar());
+        hamburguesa.agregarExtra(new Ketchup("poco"));
+        hamburguesa.agregarExtra(new Mayonesa("mucho"));
+        hamburguesa.agregarExtra(new Huevo());
+
+        hamburguesa.agregarExtra(new Cheddar());
+        hamburguesa.agregarExtra(new Ketchup("poco"));
+        hamburguesa.agregarExtra(new Mayonesa("mucho"));
+        hamburguesa.agregarExtra(new Huevo());
+
+        this.tieneHamburguesa = true;
+    }
+
+    public Hamburguesa entregar() {
+        this.tieneHamburguesa = false;
+
+        return hamburguesa;
+    }
+
+}
