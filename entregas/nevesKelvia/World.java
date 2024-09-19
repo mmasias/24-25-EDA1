4621c57f456f56f8d7649a8f0df80c0325bad47e
kelvia.barros <barroskelvia122@gmail.com>
2024-09-19 22:42:40 +0200

    Starting the clases basing on the original code and creating a POO version. I created functions on the World class to validate the position, to clean, to verify the dirtiness (working on that), to contaminate and print the world.

--- entregas/nevesKelvia/World.java
+++ entregas/nevesKelvia/World.java
@@ -1,5 +1,57 @@
 package entregas.nevesKelvia;
 
-public class World{
+public class World {
+    private Tile[][] surface;
 
+    public World(int rows, int columns) {
+        surface = new Tile[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                surface[row][column] = new Tile();
+            }
+        }
+    }
+
+    public boolean isValidPosition(int[] position) {
+        return position[0] >= 0 && position[0] < surface.length &&
+                position[1] >= 0 && position[1] < surface[0].length;
+    }
+
+    public void cleanTile(int[] position) {
+        surface[position[0]][position[1]].clean();
+    }
+
+    public boolean isDirty() {
+        for (Tile[] row : surface) {
+            for (Tile tile : row) {
+                if (tile.isDirty()) {
+                    return ;
+                }
+            }
+        }
+    }
+
+    public void contaminate() {
+        for (Tile[] row : surface) {
+            for (Tile tile : row) {
+                tile.randomizeDirtiness();
+            }
+        }
+    }
+
+    public void printWorld(int[] vacuumPosition) {
+        cleanScreen();
+        System.out.println("---".repeat(surface[0].length));
+        for (int row = 0; row < surface.length; row++) {
+            for (int column = 0; column < surface[row].length; column++) {
+                if (vacuumPosition[0] == row && vacuumPosition[1] == column) {
+                    System.out.print("(O)");
+                } else {
+                    System.out.print(surface[row][column].getTileRepresentation());
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("---".repeat(surface[0].length));
+    }
 }
