e99b164c9e7cb1bda69d84950ad59ea58ab27969
franvenero <franvenero8@gmail.com>
2024-09-20 19:14:50 +0200

    Correción codigo Aspiradora con documentacion en README.

--- entregas/veneroFrancisco/ejercicio 1/aspiradora/Aspiradora.java
+++ entregas/veneroFrancisco/ejercicio 1/aspiradora/Aspiradora.java
@@ -2,26 +2,7 @@ public class Aspiradora {
 
     public static void main(String[] args) {
 
-        final int TILE_CLEAN = 0;
-        final int TILE_DIRTY = 1;
-        final int TILE_DIRTIER = 2;
-        final int TILE_VERY_DIRTY = 3;
-        final int TILE_EXTREMELY_DIRTY = 4;
-        final int X_AXIS = 0;
-        final int Y_AXIS = 1;
-
-        int[][] surface = {
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        };
+        int [][] surface = new int [10][25];
         int vacuumPosition[] = { 0, 0 };
 
         contaminate(surface);
@@ -38,7 +19,8 @@ public class Aspiradora {
 
     static void moveVacuum(int[] vacuumPosition, int[][] surface) {
 
-        int[] previousPosition = new int[] { vacuumPosition[0], vacuumPosition[1] };
+        int[] previousPosition = new int[2];
+        System.arraycopy(vacuumPosition, 0, previousPosition, 0, 2);
 
         int[][] directions = {
                 { -1, 1 }, { 0, 1 }, { 1, 1 },
@@ -50,15 +32,17 @@ public class Aspiradora {
         vacuumPosition[0] = vacuumPosition[0] + movement[0];
         vacuumPosition[1] = vacuumPosition[1] + movement[1];
 
+
         if (invalidPosition(vacuumPosition, surface)) {
-            vacuumPosition[0] = previousPosition[0];
-            vacuumPosition[1] = previousPosition[1];
+            System.arraycopy(previousPosition, 0, vacuumPosition, 0, 2);
+
         }
     }
 
     static boolean invalidPosition(int[] position, int[][] surface) {
-        return position[0] < 0 || position[0] >= surface.length ||
-                position[1] < 0 || position[1] >= surface[0].length;
+        return position[0] < 0 || position[0] >= surface.length || position[1] < 0 || position[1]
+        >= surface[0].length;
+
     }
 
     static void cleanWorld(int[][] surface, int[] vacuumPosition) {
@@ -68,7 +52,14 @@ public class Aspiradora {
     }
 
     static boolean isDirty(int[][] surface) {
-        return true;
+        for (int i = 0; i < surface.length; i++){
+            for (int j = 0; j < surface[i].length; j++) {
+                if (surface[i][i] != 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     static void printWorld(int[][] aMap, int[] vacuumPosition) {
