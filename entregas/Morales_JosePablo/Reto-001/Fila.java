1d6dcf25f67ce581bfe8ffd66189b1925122ac92
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 16:02:38 +0200

    Añade los métodos clienteAburrido, mostrarFila y tambien importa importa java.util.Random.

--- entregas/Morales_JosePablo/Reto-001/Fila.java
+++ entregas/Morales_JosePablo/Reto-001/Fila.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Fila {
 
     private Cliente[] clientes;
@@ -26,4 +28,28 @@ public void clienteAburrido () {
         System.out.println("La fila está vacía.");
     }
 }
+public void clienteAburrido() {
+    if (cantidadClientes > 0) {
+        Random random = new Random();
+        int index = random.nextInt(cantidadClientes);
+        Cliente aburrido = clientes[index];
+        System.arraycopy(clientes, index + 1, clientes, index, cantidadClientes - index - 1);
+        cantidadClientes--;
+        System.out.println(aburrido.mostrarCliente() + " se ha aburrido y se fue.");
+    } else {
+        System.out.println("La fila está vacía.");
     }
+}
+
+public void mostrarFila() {
+    if (cantidadClientes == 0) {
+        System.out.println("La fila está vacía.");
+    } else {
+        System.out.print("Clientes en la fila: ");
+        for (int i = 0; i < cantidadClientes; i++) {
+            System.out.print(clientes[i].mostrarCliente() + " ");
+        }
+        System.out.println();
+    }
+}
+}
