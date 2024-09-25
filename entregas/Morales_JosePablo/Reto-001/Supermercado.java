536925088ad0335718f12227990e441c136794a8
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 16:26:55 +0200

    Agrego al codigo la inicialización de filas y uso de Random

--- entregas/Morales_JosePablo/Reto-001/Supermercado.java
+++ entregas/Morales_JosePablo/Reto-001/Supermercado.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Supermercado {
 
     private Fila fila1;
@@ -9,4 +11,24 @@ public class Supermercado {
         fila2 = new Fila(10);
         contadorClientes = 1;
     }
+}
+
+public void inicializarFilas(int maxClientes) {
+    Random random = new Random();
+    int clientesFila1 = random.nextInt(maxClientes) + 1;
+    int clientesFila2 = random.nextInt(maxClientes) + 1;
+
+    int contadorClientes;
+    for (int i = 0; i < clientesFila1; i++) {
+        Fila fila1;
+        fila1.agregarCliente(new Cliente(contadorClientes++));
+    }
+
+    for (int i = 0; i < clientesFila2; i++) {
+        Fila fila2;
+        fila2.agregarCliente(new Cliente(contadorClientes++));
+    }
+
+    System.out.println("Filas inicializadas.");
+    mostrarFilas();
 }
