0634dc5704f1f2bedcadab9d16d9a193d8f095c6
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 15:43:34 +0200

    Agrega el método clienteAburrido a la clase Fila.

--- entregas/Morales_JosePablo/Reto-001/Fila.java
+++ entregas/Morales_JosePablo/Reto-001/Fila.java
@@ -15,3 +15,15 @@ public class Fila {
             System.out.println("La fila está llena.");
         }
     }
+
+public void clienteAburrido () {
+    if (cantidadClientes > 0) {
+        Cliente atendido = clientes[0];
+        System.arraycopy(clientes, 1, clientes, 0, cantidadClientes - 1);
+        cantidadClientes--;
+        System.out.println(atendido.mostrarCliente() + " ha sido atendido.");
+    } else {
+        System.out.println("La fila está vacía.");
+    }
+}
+    }
