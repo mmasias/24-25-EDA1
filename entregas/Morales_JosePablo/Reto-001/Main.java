eb739280b8acef9715bd15ad9d7e4e9b10ddd339
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 17:04:23 +0200

    Ultimo commit de la clase Main

--- entregas/Morales_JosePablo/Reto-001/Main.java
+++ entregas/Morales_JosePablo/Reto-001/Main.java
@@ -1,3 +1,14 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Supermercado supermercado = new Supermercado();
+        supermercado.inicializarFilas(10);
+
+        supermercado.atenderFila1();
+        supermercado.aburrirseFila2();
+
+        supermercado.colarseLicitamenteFila1();
+        supermercado.colarseIlicitamenteFila2();
+
+        supermercado.mostrarFilas();
+    }
 }
