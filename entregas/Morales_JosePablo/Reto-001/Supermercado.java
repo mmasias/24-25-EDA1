76682a38e44f273fb98729a5721a9e601e0a7435
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 17:42:57 +0200

    Correccion de los errrores de la clase Supermercado

--- entregas/Morales_JosePablo/Reto-001/Supermercado.java
+++ entregas/Morales_JosePablo/Reto-001/Supermercado.java
@@ -11,58 +11,63 @@ public class Supermercado {
         fila2 = new Fila(10);
         contadorClientes = 1;
     }
-}
 
-public void inicializarFilas(int maxClientes) {
-    Random random = new Random();
-    int clientesFila1 = random.nextInt(maxClientes) + 1;
-    int clientesFila2 = random.nextInt(maxClientes) + 1;
+    public void inicializarFilas(int maxClientes) {
+        Random random = new Random();
+        int clientesFila1 = random.nextInt(maxClientes) + 1;
+        int clientesFila2 = random.nextInt(maxClientes) + 1;
 
-    int contadorClientes;
-    for (int i = 0; i < clientesFila1; i++) {
-        Fila fila1;
+        for (int i = 0; i < clientesFila1; i++) {
+            fila1.agregarCliente(new Cliente(contadorClientes++));
+        }
+
+        for (int i = 0; i < clientesFila2; i++) {
+            fila2.agregarCliente(new Cliente(contadorClientes++));
+        }
+
+        System.out.println("Filas inicializadas.");
+        mostrarFilas();
+    }
+
+    public void mostrarFilas() {
+        System.out.println("Fila 1:");
+        fila1.mostrarFila();
+        System.out.println("Fila 2:");
+        fila2.mostrarFila();
+    }
+
+    public void atenderFila1() {
+        fila1.clienteAburrido();
+    }
+
+    public void atenderFila2() {
+        fila2.clienteAburrido();
+    }
+
+    public void aburrirseFila1() {
+        fila1.clienteAburrido();
+    }
+
+    public void aburrirseFila2() {
+        fila2.clienteAburrido();
+    }
+
+    public void colarseLicitamenteFila1() {
         fila1.agregarCliente(new Cliente(contadorClientes++));
+        System.out.println("Un cliente se ha colado lícitamente en la fila 1.");
     }
 
-    for (int i = 0; i < clientesFila2; i++) {
-        Fila fila2;
+    public void colarseIllicitamenteFila2() {
         fila2.agregarCliente(new Cliente(contadorClientes++));
+        System.out.println("Un cliente se ha colado ilícitamente en la fila 2.");
     }
 
-    System.out.println("Filas inicializadas.");
-    mostrarFilas();
-}
-
-public void atenderFila1() {
-    Object fila1;
-    ((Object) fila1).atenderCliente();
-}
-
-public void atenderFila2() {
-    Object fila2;
-    ((Object) fila2).atenderCliente();
-}
-
-public void aburrirseFila1() {
-    Fila fila1;
-    fila1.clienteAburrido();
-}
-
-public void aburrirseFila2() {
-    Fila fila2;
-    fila2.clienteAburrido();
-}
-
-public void colarseLicitamenteFila1() {
-    Fila fila1;
-    int contadorClientes;
-    fila1.agregarCliente(new Cliente(contadorClientes++));
-    System.out.println("Un cliente se ha colado lícitamente en la fila 1.");
-}
-
-public void colarseIlicitamenteFila2() {
-    Fila fila2;
-    int contadorClientes;
-    fila2.agregarCliente(new Cliente(contadorClientes++));
-    System.out.println("Un cliente se ha colado ilícitamente en la fila 2.");
+    public static void main(String[] args) {
+        Supermercado supermercado = new Supermercado();
+        supermercado.inicializarFilas(10);
+        supermercado.mostrarFilas();
+        
+        supermercado.colarseIllicitamenteFila2();
+        supermercado.mostrarFilas();
+    }
 }
