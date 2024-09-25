aa3ac7dfb0230d31fdf26d005132a00c82c8c005
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 15:33:28 +0200

    Añade la clase Fila y el método para agregar clientes.

--- entregas/Morales_JosePablo/Reto-001/Fila.java
+++ entregas/Morales_JosePablo/Reto-001/Fila.java
@@ -1,10 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Fila {
-    private List<Cliente> clientes;
 
-    public Fila() {
-        clientes = new ArrayList<>();
+    private Cliente[] clientes;
+    private int cantidadClientes;
+
+    public Fila(int maxClientes) {
+        clientes = new Cliente[maxClientes];
+        cantidadClientes = 0;
     }
-}
+
+    public void agregarCliente(Cliente cliente) {
+        if (cantidadClientes < clientes.length) {
+            clientes[cantidadClientes++] = cliente;
+        } else {
+            System.out.println("La fila está llena.");
+        }
+    }
