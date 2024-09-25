e2b90660dd78bd744374d595c04a68ed10b4596a
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-25 14:48:52 +0200

    Clase Cliente creada con un ID y un método para mostrar su información

--- entregas/Morales_JosePablo/Reto-001/Cliente.java
+++ entregas/Morales_JosePablo/Reto-001/Cliente.java
@@ -1,3 +1,15 @@
 public class Cliente {
-    
+    private int id;
+
+    public Cliente(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String mostrarCliente() {
+        return "Cliente-" + id;
+    }
 }
