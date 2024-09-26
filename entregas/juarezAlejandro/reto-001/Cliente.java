29d7761eb6ec22088ead20eeab51100c5ff56a86
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-09-26 12:44:20 +0200

    Primeros dos puntos y creación de las clases que se usarán
    Se crean las clases, simulacion, fila y cliente las cuales se usarán para este reto

---
+++ entregas/juarezAlejandro/reto-001/Cliente.java
@@ -0,0 +1,24 @@
+class Cliente {
+
+    private String nombre;
+    private int id;
+
+    public Cliente(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (ID: " + id + ")";
+    }
+
+}
---
+++ entregas/juarezAlejandro/reto-001/Simulacion.java
@@ -0,0 +1,12 @@
+class Simulacion {
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+
+        for (int i = 1; i <= 10; i++) {
+            Cliente nuevoCliente = new Cliente("Cliente" + i, i);
+            fila.agregarCliente(nuevoCliente);
+        }
+
+        fila.mostrarFila();
+    }
+}
