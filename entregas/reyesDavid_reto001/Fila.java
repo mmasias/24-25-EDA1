2e7f54cf4c73ee579ae67b2d8d0e7eb8300884ea
Dreyes2907 <dreyb05@gmail.com>
2024-09-26 18:31:20 +0200

    Segundo Commit,
    Se trabaja en las 2 clases para crear el tamaño de la fila y como funcionaria el mundo

--- entregas/reyesDavid_reto001/Fila.java
+++ entregas/reyesDavid_reto001/Fila.java
@@ -1,5 +1,44 @@
 package entregas.reyesDavid_reto001;
 
-public class Fila {
-    
+class Fila {
+
+    private Cliente[] fila;
+    private int capacidad;
+    private int numClientes;
+
+    public Fila() {
+        this.capacidad = 10;
+        this.fila = new Cliente[capacidad];
+        this.numClientes = 0;
+    }
+
+    public boolean agregarCliente(Cliente cliente) {
+        if (numClientes < capacidad) {
+            fila[numClientes] = cliente;
+            numClientes++;
+            return true;
+        } else {
+            System.out.println("Fila llena");
+            return false;
+        }
+    }
+
+    public void mostrarFila() {
+        if (numClientes == 0) {
+            System.out.println("Fila vacia");
+        } else {
+            System.out.println("Cliente en fila");
+            for (int i = 0; i < numClientes; i++) {
+                System.out.println(fila[i]);
+            }
+        }
+    }
+
+    public int getNumClientes() {
+        return numClientes;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
 }
--- entregas/reyesDavid_reto001/Mundo.java
+++ entregas/reyesDavid_reto001/Mundo.java
@@ -1,5 +1,14 @@
 package entregas.reyesDavid_reto001;
 
-public class Mundo {
-    
+class Mundo {
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+
+        for (int i = 1; i <= 10; i++) {
+            Cliente nuevoCliente = new Cliente("Cliente" + i, i);
+            fila.agregarCliente(nuevoCliente);
+        }
+
+        fila.mostrarFila();
+    }
 }
