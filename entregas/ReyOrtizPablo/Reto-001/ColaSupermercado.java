dd0bafd3be60cb71aefc7fd2a355a16717619781
pareyor <pablo.reyortiz@alumnos.uneatlantico.com>
2024-09-26 16:07:36 +0200

    iniciamos la cola y verificamos si hay clientes

--- entregas/ReyOrtizPablo/Reto-001/ColaSupermercado.java
+++ entregas/ReyOrtizPablo/Reto-001/ColaSupermercado.java
@@ -7,6 +7,21 @@ public class ColaSupermercado {
         cola = new String[MAX_COLA];
         numClientes = 0; 
     }
+    public void inicializarCola() {
+        for (int i = 0; i < MAX_COLA; i++) {
+            cola[i] = null;
+        }
+        numClientes = 0;
+        System.out.println("La cola se ha iniciado.");
+    }
+private boolean hayCliente(String cliente) {
+        for (int i = 0; i < numClientes; i++) {
+            if (cola[i].equals(cliente)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
