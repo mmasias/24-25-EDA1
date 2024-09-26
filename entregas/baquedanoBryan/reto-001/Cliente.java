a8b8730954e58b626ddcf31f7dfedf03de7876d7
enmabry <bryanbaquedano11@gmail.com>
2024-09-26 23:56:53 +0200

    Agregué métodos
    agregué el método getNumItems que devuelve el número de items del cliente, un método que aumenta la cantidad, y solo sí es mayor que 0, y un último método que muestra la información del cliente.

--- entregas/baquedanoBryan/reto-001/Cliente.java
+++ entregas/baquedanoBryan/reto-001/Cliente.java
@@ -6,4 +6,21 @@ public class Cliente{
         this.numItems = numItems;
     }
 
+    public int getNumItems(){
+        return numItems;
+    }
+
+    public void aumentarItems(int cantidad) {
+        if (cantidad > 0) {
+            this.numItems += cantidad;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente con " + numItems + " ítems.";
+    }
 }
+
+
+
