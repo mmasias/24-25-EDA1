5f2fbd1944ec3ec3a652bbc8d0763914f47f3ce3
kelvia.barros <barroskelvia122@gmail.com>
2024-09-26 23:06:24 +0200

    He actualizado la clase Persona y cada una tiene su ID unico

--- entregas/nevesKelvia/Persona.java
+++ entregas/nevesKelvia/Persona.java
@@ -1,5 +1,12 @@
 package entregas.nevesKelvia;
 
-public class Persona {
-    
+class Persona {
+    private static int contador = 0;
+    private int id;
+
+    public Persona() {
+        this.id = ++contador;
+        System.out.println("Se ha creado la persona con ID: " + id);
+    }
 }
+
