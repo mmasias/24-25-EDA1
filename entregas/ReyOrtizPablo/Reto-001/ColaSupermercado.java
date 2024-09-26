18caabc620d1e0af2fcdc8366a4536c9ab5202b5
pareyor <pablo.reyortiz@alumnos.uneatlantico.com>
2024-09-26 16:01:28 +0200

    definimos la capacidad maxima de la cola

--- entregas/ReyOrtizPablo/Reto-001/ColaSupermercado.java
+++ entregas/ReyOrtizPablo/Reto-001/ColaSupermercado.java
@@ -1,3 +1,13 @@
 
 public class ColaSupermercado {
+    private static final int MAX_COLA = 12;
+    private String[] cola;
+    private int numClientes;
+    public ColaSupermercado() {
+        cola = new String[MAX_COLA];
+        numClientes = 0; 
+    }
+
+
+
 }
