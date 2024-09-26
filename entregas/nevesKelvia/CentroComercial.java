37325b0a16e4eee6227cdc658df3ad6f354a5d2f
kelvia.barros <barroskelvia122@gmail.com>
2024-09-26 21:13:07 +0200

    Cree un array para representar la fila de personas. Habrá un máximo de 100 personas  en la fila. He añadido la función recibe que recibe una nueva persona en la fila y un alerta caundo haya más que 10 personas en la fila.

--- entregas/nevesKelvia/CentroComercial.java
+++ entregas/nevesKelvia/CentroComercial.java
@@ -1,5 +1,25 @@
 package entregas.nevesKelvia;
 
-public class CentroComercial {
-    
+class CentroComercial {
+    private Persona[] fila;
+    private int ultimo;
+
+    public CentroComercial() {
+        fila = new Persona[100];
+        ultimo = 0;
+    }
+
+    public void recibe(Persona persona) {
+        if (ultimo < fila.length) {
+            fila[ultimo] = persona;
+            ultimo++;
+            System.out.println("Ha llegado una nueva persona a la fila.");
+        } else {
+            System.out.println("La fila está llena.");
+        }
+
+        if (ultimo > 10) {
+            System.out.println("¡Pasen por esta caja en orden de fila! Hay muchas personas en la fila.");
+        }
+    }
 }
