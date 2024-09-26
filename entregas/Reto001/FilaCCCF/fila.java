fd4117fbe61789bbe7aeb54ad1b51b95b6dcc9b0
Sergiorl2 <145299767+Sergiorl2@users.noreply.github.com>
2024-09-26 22:48:10 +0200

    Creación de funciones
    creé el constructor, agregue las funciones que voy a necesitar y modifiqué el orden de las carpetas de entrega

---
+++ entregas/Reto001/FilaCCCF/fila.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+public class Fila {
+
+    public Fila(int capacidad) {
+        this.capacidad = capacidad;
+        this.personas = new String[capacidad];
+        this.contador = 0;
+    }
+    public void abrirFila() {
+
+    }
+    public void personaLlega(String nombre) {
+
+    }
+    public void atenderPersona() {
+
+    }
+    public void personaSeVa(String nombre) {
+
+    }
+    public void traerCosas(String nombre) {
+
+    }
+    public void colarseLicitamente(String nombre) {
+
+    }
+    public void mensajeMuchaGente() {
+
+    }
+    public void simularAcciones() {
+
+    }
