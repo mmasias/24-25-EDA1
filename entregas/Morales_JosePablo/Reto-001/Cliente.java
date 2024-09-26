932d78f45754a59ea278c34083e433ae212be848
JPMorales04 <jpmorales.epic@gmail.com>
2024-09-26 14:16:41 +0200

    Agregué el método getId() en la clase Cliente para acceder al ID del cliente

--- entregas/Morales_JosePablo/Reto-001/Cliente.java
+++ entregas/Morales_JosePablo/Reto-001/Cliente.java
@@ -1,12 +1,9 @@
 public class Cliente {
+
     private int id;
 
     public Cliente(int id) {
         this.id = id;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String mostrarCliente() {
