28788f470da416dbe8f8d8a27d3f873706095157
Dreyes2907 <dreyb05@gmail.com>
2024-09-26 19:19:07 +0200

    Commit finl.
    Fin del codigo con lo que se pide y esta completo

--- entregas/reyesDavid_reto001/Mundo.java
+++ entregas/reyesDavid_reto001/Mundo.java
@@ -5,23 +5,17 @@ class Mundo {
         Fila fila = new Fila();
 
         for (int i = 1; i <= 10; i++) {
-            Cliente nuevoCliente = new Cliente("Cliente" + i, i);
+            int numItems = (int) (Math.random() * 11);
+            Cliente nuevoCliente = new Cliente("Cliente" + i, i, numItems);
             fila.agregarCliente(nuevoCliente);
         }
 
-        System.out.println("Fila inicial");
-        fila.mostrarFila();
-
-        System.out.println("\nAtendiendo al primer cliente");
-        fila.atenderCliente();
-
-        System.out.println("\nFila después de atender al primer cliente:");
-        fila.mostrarFila();
-
-        System.out.println("\nSimulando que un cliente se aburre...");
-        fila.clienteAburrido();
 
         System.out.println("\nFila después de que un cliente se aburre:");
+        fila.mostrarFila();
+        System.out.println("\nSimulando que a un cliente le traen ítems...");
+        fila.entregarItems();
+        System.out.println("\nFila después de entregar ítems:");
         fila.mostrarFila();
     }
 }
--- entregas/reyesDavid_reto001/cliente.java
+++ entregas/reyesDavid_reto001/cliente.java
@@ -4,22 +4,30 @@ class Cliente {
 
     private String nombre;
     private int id;
+    private int numItems;
 
     public Cliente(String nombre, int id) {
+    public Cliente(String nombre, int id, int numItems) {
         this.nombre = nombre;
         this.id = id;
+        this.numItems = numItems;
     }
 
     public String getNombre() {
-        return nombre;
-    }
-
-    public int getId() {
         return id;
     }
 
+    public int getNumItems() {
+        return numItems;
+    }
+    public void aumentarItems(int cantidad) {
+        this.numItems += cantidad;
+    }
+    @Override
     public String toString() {
-        return nombre + " (ID: " + id + ")";
+       
+        return nombre + " (ID: " + id + ") espera con " + numItems + " ítems";
     }
 
+}
 }
