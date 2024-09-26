092f17c1b37743ab4f51e58860b7dc185e8603fa
enmabry <bryanbaquedano11@gmail.com>
2024-09-27 00:32:32 +0200

    Agregué el constructor y un método
    Agregué el constructor que inicializa la fila, tendrá una capacidad de 15, y al iniciar no hay ningun cliente, también hice el método de agregar cliente.

--- entregas/baquedanoBryan/reto_001/Fila.java
+++ entregas/baquedanoBryan/reto_001/Fila.java
@@ -6,7 +6,24 @@ public class Fila {
     private int capacidad;
     private int numClientes;
 
-    
+    public Fila(){
+        this.capacidad = 15;
+        this.fila = new Cliente[capacidad];
+        this.numClientes = 0;
+    }
+
+    public boolean agregarCliente(Cliente cliente) {
+        if (numClientes < capacidad) { 
+            fila[numClientes] = cliente; 
+            numClientes++;
+            return true; 
+        } else {
+            System.out.println("Fila llena."); 
+            return false; 
+        }
+    }
+
+
 
     
 }
