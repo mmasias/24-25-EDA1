9852c86054f16bf58cedec8fece8f3da166e966e
Raúl García Roldán <144956120+xImRaul@users.noreply.github.com>
2024-09-26 16:28:51 +0200

    Update Fila.java

--- entregas/garciaRaul/Reto-001/Fila.java
+++ entregas/garciaRaul/Reto-001/Fila.java
@@ -27,3 +27,31 @@ public class Fila {
     private boolean hayClientes() {
         return clientes[0]!=null;
     }
+
+    private void liberarEspacio(Cliente[] clientes, int indice, int elementosOcupados){
+        assert indice >= 0 && indice < elementosOcupados;
+
+        for (int i = indice; i < elementosOcupados - 1; i++) {
+            clientes[i] = clientes[i + 1];
+        }
+
+        clientes[elementosOcupados - 1] = null;
+
+    }
+
+    public void clienteSeVa(){
+        if (posicionUltimo>0) {
+            liberarEspacio(clientes, new Random().nextInt(posicionUltimo), posicionUltimo+1);
+            posicionUltimo--;
+        }
+    }
+
+    public void recibirItemsExternos() {
+        if(posicionUltimo > 0 && new Random().nextInt(100) < 20){ 
+            int clienteSeleccionado = new Random().nextInt(posicionUltimo); 
+            int itemsExtra = 2 + new Random().nextInt(4);
+            clientes[clienteSeleccionado].añadirItems(itemsExtra);
+        }
+    }
+    
+}
