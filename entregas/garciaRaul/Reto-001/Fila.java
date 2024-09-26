aa5f16081f4f605378995622d9eed3a83cb483e8
Raúl García Roldán <144956120+xImRaul@users.noreply.github.com>
2024-09-26 16:22:07 +0200

    Agregando metodos y atributos a la clase Fila.java

--- entregas/garciaRaul/Reto-001/Fila.java
+++ entregas/garciaRaul/Reto-001/Fila.java
@@ -1,1 +1,29 @@
+import java.util.Random;
 
+public class Fila {
+
+    private Cliente[] clientes;
+    private int posicionUltimo;
+
+    public Fila(){
+
+        clientes = new Cliente[150];
+        posicionUltimo = 0;
+
+    }
+
+    public void añadirCliente(){
+        clientes[posicionUltimo] = new Cliente();
+        posicionUltimo++;
+    }
+
+    public void atenderCliente(){
+        if(hayClientes()){
+            liberarEspacio(clientes, 0, posicionUltimo+1);
+            posicionUltimo--;
+        }
+    }
+
+    private boolean hayClientes() {
+        return clientes[0]!=null;
+    }
