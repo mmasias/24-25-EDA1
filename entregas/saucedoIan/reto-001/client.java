127ebc020a038aaa2e398c75b376a5dd346c54fa
Ian Saucedo <s.ians2212@gmail.com>
2024-09-26 18:34:19 +0200

    first outline of the class client

--- entregas/saucedoIan/reto-001/client.java
+++ entregas/saucedoIan/reto-001/client.java
@@ -1,3 +1,18 @@
 class Client {
+  private int id;
+  private int items;
+
+  public Client(int id, int items) {
+    this.id = id;
+    this.items = items;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public boolean hasItems() {
+    return items > 0;
+  }
 
 }
