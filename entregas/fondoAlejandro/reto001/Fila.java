329ca720ea15672ea8a4c304c9ba9c29f30ab2d0
alejandrofondo <jose.fondo@alumnos.uneatlantico.es>
2024-09-26 22:14:49 +0200

    Creación del método llegaCliente, atenderCliente, clienteSeMarcha y mostarFila.

--- entregas/fondoAlejandro/reto001/Fila.java
+++ entregas/fondoAlejandro/reto001/Fila.java
@@ -17,4 +17,42 @@ public class Fila {
         }
         System.out.println("Se abre una fila para atender a los clientes del centro comercial");
     }
+
+    public void llegaCliente(){
+        if (tamaño < longitudFila) {
+            fila[tamaño] = "[O]"; 
+            tamaño++;
+            System.out.println("Un cliente ha llegado a la fila.");
+            mostrarFila();
+        } else {
+            System.out.println("La fila esta llena." + "\n");
+        }
+    }
+
+    public void atenderCliente(){
+        if (tamaño > 0) {
+            System.out.println("El primer cliente de la fila ha sido atendido y se marcha.");
+            clienteSeMarcha();
+            mostrarFila();
+        } else {
+            System.out.println("No hay nadie en la fila."+"\n");
+        }
+    }
+
+    public void clienteSeMarcha(){
+        for (int i = 1; i < tamaño; i++) {
+            fila[i-1] = fila[i];
+        }
+        fila[tamaño - 1] = "[ ]"; 
+        tamaño--;
+        mostrarFila();
+    }
+
+    public void mostrarFila(){
+        System.out.print("Fila 1: " );
+        for (int i = 0; i < fila.length; i++) {
+            System.out.print(fila[i]);
+        }
+        System.out.println("\n");
+    }
 }
