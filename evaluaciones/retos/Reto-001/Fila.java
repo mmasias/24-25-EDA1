3066f871141d7a2daffd47c7726a04f517373553
Raul <raul.garcia3@alumnos.uneatlantico.es>
2024-09-26 19:36:13 +0200

    codigo mejorado

--- evaluaciones/retos/Reto-001/Fila.java
+++ evaluaciones/retos/Reto-001/Fila.java
@@ -1,99 +1,57 @@
-class Fila {
-    private String[] fila;
-    private int capacidadMaxima;
-    private int numPersonas;
+import java.util.Random;
 
-    public Fila(int capacidadMaxima) {
-        this.capacidadMaxima = capacidadMaxima;
-        this.fila = new String[capacidadMaxima];
-        this.numPersonas = 0;
+public class Fila {
+
+    private Cliente[] clientes;
+    private int posicionUltimo;
+
+    public Fila(){
+
+        clientes = new Cliente[150];
+        posicionUltimo = 0;
+
     }
 
-    public void abrirFila() {
-        for (int i = 0; i < capacidadMaxima; i++) {
-            fila[i] = null;
-        }
-        numPersonas = 0;
-        System.out.println("Se ha abierto una nueva fila.");
+    public void añadirCliente(){
+        clientes[posicionUltimo] = new Cliente();
+        posicionUltimo++;
     }
 
-    public void llegarGente() {
-        if (numPersonas < capacidadMaxima) {
-            fila[numPersonas] = "Persona";
-            numPersonas++;
-            System.out.println("Ha llegado una persona a la fila. Personas en fila: " + numPersonas);
-        } else {
-            System.out.println("La fila está llena. No puede entrar más gente.");
+    public void atenderCliente(){
+        if(hayClientes()){
+            liberarEspacio(clientes, 0, posicionUltimo+1);
+            posicionUltimo--;
         }
     }
 
-    public void pasarASerAtendido() {
-        if (numPersonas > 0) {
-            System.out.println("Una persona ha pasado a ser atendida.");
+    private boolean hayClientes() {
+        return clientes[0]!=null;
+    }
 
-            for (int i = 1; i < numPersonas; i++) {
-                fila[i - 1] = fila[i];
-            }
-            fila[numPersonas - 1] = null;
-            numPersonas--;
-            System.out.println("Personas en fila: " + numPersonas);
-        } else {
-            System.out.println("La fila está vacía, nadie para ser atendido.");
+    private void liberarEspacio(Cliente[] clientes, int indice, int elementosOcupados){
+        assert indice >= 0 && indice < elementosOcupados;
+
+        for (int i = indice; i < elementosOcupados - 1; i++) {
+            clientes[i] = clientes[i + 1];
+        }
+
+        clientes[elementosOcupados - 1] = null;
+
+    }
+
+    public void clienteSeVa(){
+        if (posicionUltimo>0) {
+            liberarEspacio(clientes, new Random().nextInt(posicionUltimo), posicionUltimo+1);
+            posicionUltimo--;
         }
     }
 
-    public void irseDeLaFila() {
-        if (numPersonas > 0) {
-            System.out.println("Una persona se ha ido de la fila.");
-
-            for (int i = 1; i < numPersonas; i++) {
-                fila[i - 1] = fila[i];
-            }
-            fila[numPersonas - 1] = null;
-            numPersonas--;
-            System.out.println("Personas en fila: " + numPersonas);
-        } else {
-            System.out.println("La fila está vacía, nadie se puede ir.");
+    public void recibirItemsExternos() {
+        if(posicionUltimo > 0 && new Random().nextInt(100) < 20){ 
+            int clienteSeleccionado = new Random().nextInt(posicionUltimo); 
+            int itemsExtra = 2 + new Random().nextInt(4);
+            clientes[clienteSeleccionado].añadirItems(itemsExtra);
         }
     }
-
-    public void colarseLicitamente() {
-        if (numPersonas < capacidadMaxima) {
-            for (int i = numPersonas; i > 1; i--) {
-                fila[i] = fila[i - 1];
-            }
-            fila[1] = "Persona";
-            numPersonas++;
-            System.out.println("Una persona se ha colado lícitamente. Personas en fila: " + numPersonas);
-        } else {
-            System.out.println("La fila está llena. No puede colarse nadie más.");
-        }
-    }
-
-    public void colarseIlicitamente() {
-        if (numPersonas < capacidadMaxima) {
-            fila[numPersonas] = "Persona";
-            numPersonas++;
-            System.out.println("Una persona se ha colado ilícitamente. Personas en fila: " + numPersonas);
-        } else {
-            System.out.println("La fila está llena. No puede colarse nadie más.");
-        }
-    }
-
-    public void anunciarOrdenFila() {
-        if (numPersonas == capacidadMaxima) {
-            System.out.println("Pasen por esta caja en orden de fila... Personas en fila: " + numPersonas);
-        }
-    }
-
-    public void mostrarFila() {
-        System.out.println("Personas en fila: " + numPersonas);
-    }
-
-    public void vaciarFila() {
-        while (numPersonas > 0) {
-            pasarASerAtendido();
-        }
-        System.out.println("La fila está vacía. Ejercicio finalizado.");
-    }
+    
 }
