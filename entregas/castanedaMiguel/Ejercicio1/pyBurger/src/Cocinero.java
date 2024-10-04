8a762a8a89042a5f8afa394c625160b91f49f6bb
micastg <micastg@unirioja.es>
2024-10-04 16:05:35 +0200

    commit

--- entregas/castanedaMiguel/Ejercicio1/pyBurger/src/Cocinero.java
+++ entregas/castanedaMiguel/Ejercicio1/pyBurger/src/Cocinero.java
@@ -1,12 +1,11 @@
 class Cocinero extends Empleado {
-    final int NUMERO_EXTRAS = 4;
-    final Carne laCarne = new Res("poco hecha");
-    final Hamburguesa hamburguesa = new Hamburguesa(new Brioche(), new Brioche(), laCarne, NUMERO_EXTRAS);
+
+
+
     public void prepararHamburguesa() {
-
-
-
-
+        final int NUMERO_EXTRAS = 4;
+        Carne laCarne = new Res("poco hecha");
+        Hamburguesa hamburguesa = new Hamburguesa(new Brioche(), new Brioche(), laCarne, NUMERO_EXTRAS);
         hamburguesa.agregarExtra(new Cheddar());
         hamburguesa.agregarExtra(new Ketchup("poco"));
         hamburguesa.agregarExtra(new Mayonesa("mucho"));
