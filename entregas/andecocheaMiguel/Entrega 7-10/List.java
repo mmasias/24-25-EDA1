cf91e74d5ab7b43b0052d0085b6f9b10c0d83990
Andecochea <andecomaf@gmail.com>
2024-10-07 20:06:46 +0200

    Entrega 7-10

---
+++ entregas/andecocheaMiguel/Entrega 7-10/List.java
@@ -0,0 +1,106 @@
+
+
+public class List {
+
+    private Node first = null;
+    private int size = 0;
+    
+
+    public int size() {
+        if (this.first == null) {
+            return 0;
+        }
+        int count = 1;
+        Node iterator = first;
+        while (iterator.getNext() != null) {
+            count++;
+            iterator = iterator.getNext();
+        }
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return this.size() > 0 ? false : true;
+    }
+
+    public void insertEnd(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null)
+            this.first = newNode;
+        else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+        size ++;
+    }
+    
+    public void insertFront(String value) {
+        Node n = new Node(value);
+        if (this.first == null)
+            this.first = n;
+        else {
+            n.setNext(this.first);
+            this.first = n;
+        }
+        size++;
+    }
+
+    public void deleteEnd() {
+        if (this.first != null) {
+            Node iterator = this.first;
+            Node previous = null;
+            while (iterator.getNext() != null) {
+                previous = iterator;
+                iterator = iterator.getNext();
+            }
+            previous.setNext(null);
+            size--;
+        }
+    }
+
+    public void deleteFront(){
+        if(this.first != null){
+            this.first = this.first.getNext();
+            size--;
+        }
+    }
+
+    public void deleteAll(){
+        first = null;
+    }
+
+    public Node[] find(String value){
+        Node[] results = new Node[this.size()];
+        int count = 0;
+        Node iterator = this.first;
+        while (iterator != null) {
+            if (iterator.getValue().equals(value)) {
+                results[count++] = iterator;
+            }
+            iterator = iterator.getNext();
+        }
+        Node[] finalResults = new Node[count];
+        for (int i = 0; i < count; i++) {
+            finalResults[i] = results[i];
+        }
+        return finalResults;
+    }
+
+
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        if (iterator == null)
+            return list;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+}
