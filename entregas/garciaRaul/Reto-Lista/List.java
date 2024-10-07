4184f45d9ff4bc2284e06e3e39bc99fb04e0bb90
Raul <raul.garcia3@alumnos.uneatlantico.es>
2024-10-07 21:16:36 +0200

    Mejora de codigo

---
+++ entregas/garciaRaul/Reto-Lista/List.java
@@ -0,0 +1,92 @@
+public class List {
+
+    private Node first = null;
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return this.size > 0;
+    }
+
+    public void insertEnd(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null){
+            this.first = newNode;
+        }else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+        size++;
+    }
+    
+    public void insertFront(String value) {
+        Node n = new Node(value);
+        if (this.first == null) {
+            this.first = n;
+        }else {
+            n.setNext(this.first);
+            this.first = n;
+        } 
+        size++;
+    }
+
+    public void deleteEnd() {
+        if (this.first != null) {
+            Node iterator = this.first;
+            Node previous = null;
+            while (iterator.getNext() != null) {
+                previous = iterator;
+                iterator = iterator.getNext();
+            }
+            previous.setNext(null);
+            size--;
+        }
+    }
+
+    public void deleteFront(){
+
+        if (this.first !=null) {
+            this.first = this.first.getNext();
+            size--;
+        }
+        
+    }
+
+    public List find(String value){
+
+        List foundList = new List();
+        if (this.first != null) {
+            Node iterator = this.first;
+    
+            while (iterator != null) {
+                if (iterator.getValue().equals(value)) {
+                        foundList.insertEnd(iterator.getValue());
+                }
+                iterator = iterator.getNext();
+            }
+    
+            return foundList;
+        }
+        return foundList;
+    }
+
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        if (iterator == null)
+            return list;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+}
