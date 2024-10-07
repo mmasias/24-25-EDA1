fb272360a10e934220fdcdb4e52b027a88a90538
jaimelinares6 <jaime.linares@alumnos.uneatlantico.es>
2024-10-07 19:58:08 +0200

    Ejercicio 7/10

---
+++ entregas/linaresJaime/ListaBasica/Ejemplo.java
@@ -0,0 +1,40 @@
+package entregas.linaresJaime.ListaBasica;
+    
+class Ejemplo {
+    public static void main(String[] args) {
+
+        List nephews = new List();
+
+        viewLine(nephews);
+        System.out.println("Esta vacia? " + nephews.isEmpty());
+
+        nephews.insertEnd("Juanito");
+        nephews.insertEnd("Pepito");
+        nephews.insertEnd("Anita");
+
+        System.out.println("Esta vacia? " + nephews.isEmpty());
+
+
+
+        nephews.insertEnd("Luisito");
+        viewLine(nephews);
+
+        System.out.println("Tamaño: " + nephews.size());
+
+        nephews.deleteEnd();
+        System.out.println("Tamaño: " + nephews.size());
+
+        viewLine(nephews);
+    }
+
+    static void viewLine(List filaDeSobrinos) {
+
+        String[] larray = filaDeSobrinos.listAll();
+        System.out.println("LA FILA" + "-".repeat(18));
+        for (String sobrino : larray){
+            System.out.println(sobrino);
+        }
+        System.out.println("-".repeat(25));
+    }
+}
+
