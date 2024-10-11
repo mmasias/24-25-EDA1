fa4110682d9440fc93b7dc95b5f556ef3387e93d
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-10-11 13:03:53 +0200

    Primer commit de la clase 11 de octubre, reto 2
    Únicamente coloqué los comentarios para poder realizar el primer commit, más adelante realizaré el reto, una dissculpa

--- entregas/juarezAlejandro/10-07/List.java
+++ entregas/juarezAlejandro/10-07/List.java
@@ -1,3 +1,15 @@
+/*Se discutió en clase qué inconvenientes tenía el resolverlo con un array
+Se acordó que la forma natural de hacerlo sería mediante otra lista, pero!
+Se vió como el manejo inadecuado de listas podía afectar (y de hecho, afectaba) la lista original.*/
+
+/*Reto
+Implemente una forma en la que se pueda responder a la búsqueda sin afectar la lista original.
+
+La vista pública del método sería:
+
+  public FoundNodeList find(String value) {}
+Que devuelve la lista de nodos que responden al criterio consultado. */
+
 public class List {
 
     private Node first = null;
