1a027630476900175042e0c5f6da3769cc55740b
Pareyor <144883777+Pareyor@users.noreply.github.com>
2024-10-11 22:40:37 +0200

    Actualizacion list.java con comentarios de los cambios

--- entregas/ReyOrtizPablo/Reto-002/List.java
+++ entregas/ReyOrtizPablo/Reto-002/List.java
@@ -1,109 +1,101 @@
-package entregas.ReyOrtizPablo.Reto0710;
-
 public class List {
-    private Node first = null;
-    private int size = 0;
 
-    public int size() {
-        return this.size;
-    }
+    private Node first = null;
 
-    public boolean isEmpty() {
-        return this.size == 0;
-    }
+    public int size() {
+        int count = 0;
+        Node iterator = first;
+        while (iterator != null) {
+            count++;
+            iterator = iterator.getNext();
+        }
+        return count;
+    }
 
-    public void insertEnd(String value) {
-        Node newNode = new Node(value);
-        if (this.first == null)
-            this.first = newNode;
-        else {
-            Node iterator = this.first;
-            while (iterator.getNext() != null) {
-                iterator = iterator.getNext();
-            }
-            iterator.setNext(newNode);
-        }
-        this.size++;
-    }
+    public boolean isEmpty() {
+        return this.first == null;
+    }
 
-    public void insertFront(String value) {
-        Node n = new Node(value);
-        if (this.first == null)
-            this.first = n;
-        else {
-            n.setNext(this.first);
-            this.first = n;
-        }
-        this.size++;
-    }
+    public void insertEnd(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null) {
+            this.first = newNode;
+        } else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+    }
 
-    public void deleteEnd() {
-        if (this.first != null) {
-            if (this.first.getNext() == null) {
-                this.first = null;
-            } else {
-                Node iterator = this.first;
-                Node previous = null;
-                while (iterator.getNext() != null) {
-                    previous = iterator;
-                    iterator = iterator.getNext();
-                }
-                previous.setNext(null);
-            }
-            this.size--;
-        }
-    }
+    public void insertFront(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null) {
+            this.first = newNode;
+        } else {
+            newNode.setNext(this.first);
+            this.first = newNode;
+        }
+    }
 
-    public void deleteFront() {
-        if (this.first != null) {
-            this.first = this.first.getNext();
-            this.size--;
-        }
-    }
+    public void deleteEnd() {
+        if (this.first == null) return;
 
-    public Node[] find(String value) {
-        Node iterator = this.first;
-        int matchCount = 0;
-        while (iterator != null) {
-            if (iterator.getValue().equals(value)) {
-                matchCount++;
-            }
-            iterator = iterator.getNext();
-        }
+        if (this.first.getNext() == null) {
+            this.first = null;
+            return;
+        }
 
-        if (matchCount == 0) {
-            return null;
-        }
+        Node iterator = this.first;
+        Node previous = null;
+        while (iterator.getNext() != null) {
+            previous = iterator;
+            iterator = iterator.getNext();
+        }
+        previous.setNext(null);
+    }
 
-        Node[] result = new Node[matchCount];
-        iterator = this.first;
-        int index = 0;
+    public void deleteFront() {
+        if (this.first != null) {
+            this.first = this.first.getNext();
+        }
+    }
 
-        while (iterator != null) {
-            if (iterator.getValue().equals(value)) {
-                result[index] = iterator;
-                index++;
-            }
-            iterator = iterator.getNext();
-        }
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
 
-        return result;
-    }
+    // Aquí es donde añades el nuevo método find:
+    public Node[] find(String value) {
+        // Creamos un arreglo temporal con un tamaño igual al número de elementos en la lista.
+        Node[] results = new Node[size()];
+        Node iterator = this.first;
+        int index = 0;
 
-    public String[] listAll() {
-        String[] list = new String[this.size];
-        Node iterator = this.first;
-        int count = 0;
-        while (iterator != null) {
-            list[count] = iterator.getValue();
-            count++;
-            iterator = iterator.getNext();
-        }
-        return list;
-    }
+        // Recorremos la lista
+        while (iterator != null) {
+            if (iterator.getValue().equals(value)) {
+                results[index] = iterator; // Guardamos el nodo si el valor coincide
+                index++;
+            }
+            iterator = iterator.getNext(); // Avanzamos al siguiente nodo
+        }
 
-    public void clearList() {
-        this.first = null;
-        this.size = 0;
+        // Creamos un arreglo final que contendrá únicamente los nodos encontrados
+        Node[] finalResults = new Node[index];
+        System.arraycopy(results, 0, finalResults, 0, index);
+
+        return finalResults; // Retornamos el arreglo con los nodos encontrados
+    }
+}
     }
 }
