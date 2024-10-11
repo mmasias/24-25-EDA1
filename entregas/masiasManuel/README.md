906e3ec1e7ee0972257814464594f7710b06318f
manuel@sdf1 <manuel@masiasweb.com>
2024-10-11 18:41:11 +0200

    bigUpdate incluyendo el ejercicio base de listas

---
+++ entregas/masiasManuel/README.md
@@ -0,0 +1,3 @@
+# Carpeta de entregas
+
+Manuel
---
+++ entregas/masiasManuel/src/reto002/List.java
@@ -0,0 +1,70 @@
+public class List {
+
+    private Node first = null;
+
+    public int size() {
+        if (this.first == null) {
+            return 0;
+        }
+        int count = 1;
+        Node iterator = first;
+        while (iterator.getNext() != null) {
+            count++;
+            iterator = iterator.getNext();
+        }
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return this.size() > 0 ? false : true;
+    }
+
+    public void insertEnd(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null)
+        this.first = newNode;
+        else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+    }
+    
+    public void insertFront(String value) {
+        Node n = new Node(value);
+        if (this.first == null)
+            this.first = n;
+        else {
+            n.setNext(this.first);
+            this.first = n;
+        }
+    }
+
+    public void delete() {
+        if (this.first != null) {
+            Node iterator = this.first;
+            Node previous = null;
+            while (iterator.getNext() != null) {
+                previous = iterator;
+                iterator = iterator.getNext();
+            }
+            previous.setNext(null);
+        }
+    }
+
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        if (iterator == null)
+            return list;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+}
