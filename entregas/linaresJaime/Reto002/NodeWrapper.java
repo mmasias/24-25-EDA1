6120aac6dc0ee5ff48bf08537036bafa84bc6218
jaimelinares6 <jaime.linares@alumnos.uneatlantico.es>
2024-10-11 19:56:09 +0200

    Commit de entrega

--- entregas/linaresJaime/Reto002/NodeWrapper.java
+++ entregas/linaresJaime/Reto002/NodeWrapper.java
@@ -1,5 +1,23 @@
 package entregas.linaresJaime.Reto002;
 
 public class NodeWrapper {
+    private Node node;
+    private NodeWrapper next;
+
+    public NodeWrapper(Node node) {
+        this.node=node;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public NodeWrapper getNext() {
+        return next;
+    }
+
+    public void setNext(NodeWrapper next) {
+        this.next = next;
+    }
     
 }
