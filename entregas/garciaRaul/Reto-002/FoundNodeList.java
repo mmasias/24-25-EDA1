358a42535e2bbe421a7e2bfa7c3092116c319dea
Raúl García Roldán <144956120+xImRaul@users.noreply.github.com>
2024-10-12 01:48:42 +0200

    Terminado la clase FoundNodeList.java

--- entregas/garciaRaul/Reto-002/FoundNodeList.java
+++ entregas/garciaRaul/Reto-002/FoundNodeList.java
@@ -1,7 +1,40 @@
-  class FoundNodeList{
-  
-    public FoundNodeList find(String value) {
+class FoundNodeList {
 
+    private Node first;
+    private int size = 0;
+
+    public int size() {
+        return size;
     }
-  
-  }
+
+    public boolean isEmpty() {
+        return this.size > 0;
+    }
+
+    public void insertEnd(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null){
+            this.first = newNode;
+        }else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+        size++;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Node iterator = this.first;
+        while (iterator != null) {
+            stringBuilder.append(iterator.getValue());
+            if (iterator.getNext() != null) {
+                stringBuilder.append(", ");
+            }
+            iterator = iterator.getNext();
+        }
+        return stringBuilder.toString();
+    }
+}
