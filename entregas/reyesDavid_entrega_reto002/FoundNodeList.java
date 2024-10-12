4b5079a72afb42ff508cc5964cc3b7c95c870899
Dreyes2907 <dreyb05@gmail.com>
2024-10-12 18:46:00 +0200

    Entrega del reto002

---
+++ entregas/reyesDavid_entrega_reto002/FoundNodeList.java
@@ -0,0 +1,33 @@
+package entregas.reyesDavid_entrega_reto002;
+
+public class FoundNodeList {
+
+    public Node[] listAll() {
+        Node[] list = new Node[this.size];
+        FoundNode iterator = this.first;
+        int index = 0;
+        while (iterator != null) {
+            list[index] = iterator.getOriginalNode();
+            index++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+
+    
+    private FoundNode first = null;
+    public int size = 0;
+    public void insert(Node node) {
+        FoundNode newNode = new FoundNode(node);
+        if (this.first == null)
+            this.first = newNode;
+        else {
+            FoundNode iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+        size++;
+    }
+}
