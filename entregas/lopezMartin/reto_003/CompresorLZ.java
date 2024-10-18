408c4ad59a29e9d476e368976ed9c52d33300414
Martín López de Novales <martin.lopez@alumnos.uneatlantico.es>
2024-10-18 13:39:07 +0200

    1er commit

---
+++ entregas/lopezMartin/reto_003/CompresorLZ.java
@@ -0,0 +1,32 @@
+package entregas.lopezMartin.reto_003;
+
+public class CompresorLZ {
+
+    List diccionario;
+    
+    public CompresorLZ(){
+        this.diccionario = new List();
+    }
+
+    public String comprime(String cadena) {
+
+        String compresion = "";
+        String diccionarioEnTexto= "";
+
+        for(int i = 0; i<cadena.length();i++){
+            if (diccionario.incluye(String.valueOf(cadena.charAt(i)))) {
+                
+                
+            }else{
+                diccionario.insertEnd(String.valueOf(cadena.charAt(i)));
+                compresion = compresion + "(" + Integer.toString(diccionario.obtenerUltimo().getIndice()) + "," + diccionario.obtenerUltimo().getValue() + ") , ";
+                diccionarioEnTexto = diccionarioEnTexto + diccionario.;
+                
+            }
+        }
+
+        return compresion + "\n" + diccionarioEnTexto;
+        
+    }
+    
+}
---
+++ entregas/lopezMartin/reto_003/List.java
@@ -0,0 +1,97 @@
+package entregas.lopezMartin.reto_003;
+
+public class List {
+
+    private Node first = null;
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return this.size > 0;
+    }
+
+    public void insertEnd(String value) {
+        Node newNode = new Node(value, this.size);
+        if (this.first == null){
+            this.first = newNode;
+        }else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+        size++;
+    }
+
+    public void deleteEnd() {
+        if (this.first != null) {
+            Node iterator = this.first;
+            Node previous = null;
+            while (iterator.getNext() != null) {
+                previous = iterator;
+                iterator = iterator.getNext();
+            }
+            previous.setNext(null);
+            size--;
+        }
+    }
+
+    public void deleteFront(){
+
+        if (this.first !=null) {
+            this.first = this.first.getNext();
+            size--;
+        }
+        
+    }
+
+    public void deleteAll(){
+        this.first = null;
+        size = 0;
+    }
+
+    public boolean incluye(String value) {
+
+        Node iterator = this.first;
+    
+        while (iterator != null) {
+            if (iterator.getValue().equals(value)) {
+                return true;
+            }
+            iterator = iterator.getNext();
+        }
+        return false;
+    }
+
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        if (iterator == null)
+            return list;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+
+    public Node obtenerUltimo(){
+        
+        if (this.first == null) {
+        return null;
+        }  
+    
+        Node iterator = this.first;
+        while (iterator.getNext() != null) {
+            iterator = iterator.getNext();
+        }
+    
+        return iterator;
+    }
+}
