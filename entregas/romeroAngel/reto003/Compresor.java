5c7d84cba5df4f6a5fe582955d4971f4cf0f2854
Angel Efrain Romero Zepeda <angel.romero@unealum.local>
2024-10-18 14:17:35 +0200

    Adicion de clase Compresor(utilizando Array Bidim) y Diccionario(utilizando Lista)

---
+++ entregas/romeroAngel/reto003/Compresor.java
@@ -0,0 +1,19 @@
+class Compresor {
+    
+    private String cadena;
+    private String[][] conversion;
+
+    public Compresor(String cadena){
+        this.cadena = cadena;
+        this.conversion = null;
+    }
+
+    public String[][] comprime(String cadena){
+        for(int i = 0;i<cadena.length();i++){
+            
+        }
+
+        return conversion;
+    }
+
+}
---
+++ entregas/romeroAngel/reto003/Diccionario.java
@@ -0,0 +1,13 @@
+class Diccionario {
+    
+    private List diccionario;
+    private int indice;
+
+
+    public Diccionario(){
+        this.diccionario = new List();
+        indice = 1;
+    }
+
+
+}
