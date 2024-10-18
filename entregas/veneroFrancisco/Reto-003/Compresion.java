2783313fef97eb31d9f9f297910e117ceac28649
franvenero <franvenero8@gmail.com>
2024-10-18 13:41:58 +0200

    Reto-003 Final

--- entregas/veneroFrancisco/Reto-003/Compresion.java
+++ entregas/veneroFrancisco/Reto-003/Compresion.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Compresion {
     public static String[] comprime(String cadena) {
-        List<String> diccionario = new ArrayList<>(); 
+        List<String> diccionario = new ArrayList<>();
         List<String> resultado = new ArrayList<>();
         int posicionActual = 0;
 
@@ -11,7 +12,6 @@ public class Compresion {
             String coincidencia = "";
             int longitud = 0;
 
-            
             for (int i = 1; i <= cadena.length() - posicionActual; i++) {
                 String ventana = cadena.substring(posicionActual, posicionActual + i);
                 if (diccionario.contains(ventana)) {
@@ -23,14 +23,18 @@ public class Compresion {
             }
 
             if (longitud > 0) {
-                
                 int indice = diccionario.indexOf(coincidencia);
-                char siguienteCaracter = cadena.charAt(posicionActual + longitud);
-                resultado.add("(" + (indice + 1) + "," + siguienteCaracter + ")");
-                posicionActual += longitud + 1;
-                diccionario.add(coincidencia + siguienteCaracter);
+
+                if (posicionActual + longitud < cadena.length()) {
+                    char siguienteCaracter = cadena.charAt(posicionActual + longitud);
+                    resultado.add("(" + (indice + 1) + "," + siguienteCaracter + ")");
+                    diccionario.add(coincidencia + siguienteCaracter);
+                    posicionActual += longitud + 1;
+                } else {
+                    resultado.add("(" + (indice + 1) + ",)");
+                    posicionActual += longitud;
+                }
             } else {
-                
                 char actual = cadena.charAt(posicionActual);
                 diccionario.add(String.valueOf(actual));
                 resultado.add("(0," + actual + ")");
@@ -42,9 +46,22 @@ public class Compresion {
     }
 
     public static void main(String[] args) {
-        String cadena = "abababc";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la cadena a comprimir:");
+        String cadena = scanner.nextLine();
+
         String[] resultado = comprime(cadena);
-        System.out.println("Resultado comprimido: " + resultado[0]);
-        System.out.println("Diccionario: " + resultado[1]);
+
+        System.out.println("El resultado comprimido es:");
+        System.out.println(resultado[0]);
+
+        System.out.println("El diccionario es:");
+        String[] diccionario = resultado[1].replace("[", "").replace("]", "").split(", ");
+        for (int i = 0; i < diccionario.length; i++) {
+            System.out.println((i + 1) + ":" + diccionario[i]);
+        }
+
+        scanner.close();
     }
 }
