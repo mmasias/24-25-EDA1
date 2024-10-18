3f1f11057bb858d25b70a80c344293a0d53601d9
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-10-18 23:24:43 +0200

    Reto 003 terminado

--- entregas/juarezAlejandro/reto-003/Main.java
+++ entregas/juarezAlejandro/reto-003/Main.java
@@ -3,6 +3,10 @@ class Main {
       String texto = "abababcbdc";
       Compresor compresor = new Compresor();
       String comprimir = compresor.comprimir(texto);
-      System.out.println(comprimir);
+      System.out.println("Texto comprimido" + comprimir);
+
+      Descompresor descompresor = new Descompresor();
+      String descomprimido = descompresor.descomprimir(comprimir, compresor.getDiccionario());
+      System.out.println("Texto descomprimido" + descompresor);
    }
 }
