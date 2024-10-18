21a256c87de3e10fc7659142135efe30c713405d
Raul <raul.garcia3@alumnos.uneatlantico.es>
2024-10-18 23:16:49 +0200

    Mejora de codigo

--- entregas/garciaRaul/Reto-003/Main.java
+++ entregas/garciaRaul/Reto-003/Main.java
@@ -1,26 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Solicitar la cadena al usuario
-        System.out.println("Ingrese la cadena a comprimir:");
-        String cadena = scanner.nextLine();
-
-        // Crear una instancia de Compresor
-        Compresor compresor = new Compresor();
-
-        // Obtener el resultado comprimido
-        String[] resultado = compresor.comprime(cadena);
-
-        // Mostrar el resultado comprimido
-        System.out.println("El resultado comprimido es: " + resultado[0]);
-
-        // Mostrar el diccionario
-        System.out.println("El diccionario es:");
-        System.out.println(resultado[1]);
+        CompresorLZW compresor = new CompresorLZW();
+        compresor.comprimir("abcabcabb");
+        compresor.comprimir("aaaa");
     }
 }
 
