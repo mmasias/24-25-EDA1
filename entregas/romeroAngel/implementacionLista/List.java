08121e5b789768e0f839a552980a4308b187db83
Angel Efrain Romero Zepeda <angel.romero@unealum.local>
2024-10-18 13:00:01 +0200

    Actualizacion de Archivos de la Implementacion de Lista

--- entregas/romeroAngel/implementacionLista/List.java
+++ entregas/romeroAngel/implementacionLista/List.java
@@ -1,14 +1,18 @@
 public class List {
 
-    private Node first = null;
-    private int size = 0;
+    private Node first;
+    private int size;
+
+    public List() {
+        this.clear();
+    }
 
     public int size() {
-        return this.size;
+        return size;
     }
 
     public boolean isEmpty() {
-        return this.size() > 0 ? false : true;
+        return this.size <= 0;
     }
 
     public void insertEnd(String value) {
@@ -22,42 +26,47 @@ public class List {
             }
             iterator.setNext(newNode);
         }
+        size++;
     }
-    
+
     public void insertFront(String value) {
-        Node n = new Node(value);
+        Node newNode = new Node(value);
         if (this.first == null)
-            this.first = n;
+            this.first = newNode;
         else {
-            n.setNext(this.first);
-            this.first = n;
+            newNode.setNext(this.first);
+            this.first = newNode;
         }
+        size++;
     }
 
     public void deleteEnd() {
         if (this.first != null) {
-            if(this.first.getNext() == null){
-                this.first = null;
-            } else {
-                Node iterator = this.first;
-                Node previous = null;
-                while (iterator.getNext() != null) {
-                    previous = iterator;
-                    iterator = iterator.getNext();
-                }
-                previous.setNext(null);
+            Node iterator = this.first;
+            Node previous = null;
+            while (iterator.getNext() != null) {
+                previous = iterator;
+                iterator = iterator.getNext();
             }
+            previous.setNext(null);
         }
+        size--;
     }
 
-    public void deleteStart() {
-        if(this.first != null){
+    public void deleteFront() {
+        if (this.first != null) {
             this.first = this.first.getNext();
+            size--;
         }
     }
 
-    public Node[] find(String value){
-        
+    public void deleteAll() {
+        this.clear();
+    }
+
+    private void clear() {
+        this.first = null;
+        this.size = 0;
     }
 
     public String[] listAll() {
@@ -73,6 +82,32 @@ public class List {
         }
         return list;
     }
+
+    public void showInConsole() {
+        System.out.println("=".repeat(25));
+        System.out.println("Fila de " + this.size());
+        System.out.println("-".repeat(25));
+        if (this.first != null) {
+            Node iterator = this.first;
+            while (iterator != null) {
+                System.out.print("[" + iterator.getValue() + "]" + iterator + " / ");
+                iterator = iterator.getNext();
+            }
+        }
+        System.out.println();
+    }
+
+    public FoundNodeList find(String value) {
+        FoundNodeList resultList = new FoundNodeList();
+        if (this.first != null) {
+            Node node = this.first;
+            while (node != null) {
+                if (node.getValue().equals(value)) {
+                    resultList.add(node);
+                }
+                node = node.getNext();
+            }
+        }
+        return resultList;
+    }    
 }
-
-
