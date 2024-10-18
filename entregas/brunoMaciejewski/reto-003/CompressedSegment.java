32a47a1eae34999eaca86ae1c467d9450ecb34d8
Bruno Maciejewski <bruno.maciejewski@alumnos.uneatlantico.es>
2024-10-18 13:49:17 +0200

    Primer commit

---
+++ entregas/brunoMaciejewski/reto-003/CompressedSegment.java
@@ -0,0 +1,7 @@
+public class CompressedSegment {
+    public CompressedSegment(int index2, char actualChar) {
+        //TODO Auto-generated constructor stub
+    }
+    private char newChar;
+    private int index;
+}
---
+++ entregas/brunoMaciejewski/reto-003/Dictionary.java
@@ -0,0 +1,14 @@
+public class Dictionary {
+    private DictionaryElement[] dictionary;
+
+    public int segmentIndex(String actualSegment) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'segmentIndex'");
+    }
+
+    public void addElement(String actualSegment) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addElement'");
+    }
+
+}
