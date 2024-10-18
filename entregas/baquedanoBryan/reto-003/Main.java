b4ff7f12ae92c68b589143828c1634c248611e52
enmabry <bryanbaquedano11@gmail.com>
2024-10-18 23:55:33 +0200

    Acabo de hacer una aproximación de lo que será la clase Main
    Ahora mismo ya podré empezar a desarrollar las clases compresor y diccionario.

--- entregas/baquedanoBryan/reto-003/Main.java
+++ entregas/baquedanoBryan/reto-003/Main.java
@@ -7,18 +7,29 @@ class Main{
         Compresor compresor = new Compresor(tamañoDiccionario);
 
         Scanner scanner = new Scanner(System.in);
+        String cadena = leerCadenaDelUsuario();
 
-        
+        if (esCadenaValida(cadena, tamañoDiccionario)) {
+            compresor.comprimir(cadena); 
+        }
 
+        scanner.close();
 
-
-
-
-
-        
     }
 
+    private static String leerCadenaDelUsuario() {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Ingrese la cadena a comprimir (máximo 100 caracteres): ");
+        return scanner.nextLine(); 
+    }
 
+    private static boolean esCadenaValida(String cadena, int maxLength) {
+        if (cadena.length() > maxLength) {
+            System.out.println("Error: La cadena no debe exceder los " + maxLength + " caracteres.");
+            return false;
+        }
+        return true; 
+    }
 
 
 }
