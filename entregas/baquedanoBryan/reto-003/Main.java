f4f8270f77677e36836fad3e6a9a1a9204acef13
enmabry <bryanbaquedano11@gmail.com>
2024-10-18 23:38:46 +0200

    Flippin on the beach (o algo así),
     Lo que haré es según las necesidades de mi clase main irán apareciendo métodos de las nuevas clases, y así hasta crear todo el programita.

--- entregas/baquedanoBryan/reto-003/Main.java
+++ entregas/baquedanoBryan/reto-003/Main.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
 
+        int tamañoDiccionario = 100;
+        Compresor compresor = new Compresor(tamañoDiccionario);
 
-
-
+        Scanner scanner = new Scanner(System.in);
 
         
+
+
+
+
+
+
         
     }
 
