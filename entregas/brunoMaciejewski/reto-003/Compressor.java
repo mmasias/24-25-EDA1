34a3d8f4903c0719dc74081f911f291c81bbd002
Bruno-MBl <bruno.maciejewski@alumnos.uneatlantico.es>
2024-10-18 16:13:41 +0200

    Zip function ready, unzip function half done

--- entregas/brunoMaciejewski/reto-003/Compressor.java
+++ entregas/brunoMaciejewski/reto-003/Compressor.java
@@ -5,25 +5,40 @@ public class Compressor {
         Dictionary dictionary=new Dictionary();
         char actualChar;
         String actualSegment="";
+        int lastIndex=0;
         boolean isNewSegment=true;
         for (int i = 0; i < toZip.length(); i++) {
             actualChar=toZip.charAt(i);
             actualSegment+=actualChar;
-            int index=dictionary.segmentIndex(actualSegment);
+            int index=dictionary.getSegmentIndex(actualSegment);
             if (index==0){
                 dictionary.addElement(actualSegment);
-                String compressedSegment=generateSegment(index,actualChar);
+                String compressedSegment=generateSegment(lastIndex,actualChar);
+                zip+=compressedSegment;
                 actualSegment="";
+                lastIndex=0;
             }else{
                 String compressedSegment=generateSegment(index,actualChar);
-                zip+=compressedSegment;
+                lastIndex=index;
             }
         }
         return zip;
     }
 
     private String generateSegment(int index, char actualChar) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generateSegment'");
+        return "("+index+","+actualChar+")";
+    }
+
+    public String unzip(String zipped) {
+        String unzip="";
+        Dictionary dictionary=new Dictionary();
+        while(!zipped.isEmpty()){
+            int index=getNextIndex(zipped);
+            char lastCharacter=getNextCharacter(zipped);
+            zipped=deleteFirstSegment(zipped);
+            if (index!=0){
+                String segment=dictionary.getSegmentAt(index);
+            }
+        }
     }
 }
--- entregas/brunoMaciejewski/reto-003/Dictionary.java
+++ entregas/brunoMaciejewski/reto-003/Dictionary.java
@@ -1,14 +1,36 @@
 public class Dictionary {
     private DictionaryElement[] dictionary;
+    
 
-    public int segmentIndex(String actualSegment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'segmentIndex'");
+    public Dictionary() {
+        dictionary=new DictionaryElement[0];
     }
 
+    public int getSegmentIndex(String actualSegment) {
+        if (dictionary.length==0) return 0;
+        for (DictionaryElement dictionaryElement : dictionary) {
+            if (dictionaryElement.getElement().equals(actualSegment)) {
+                return dictionaryElement.getIndex();
+            }
+        }
+        return 0;
+    }
+    
+
+
     public void addElement(String actualSegment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addElement'");
+        if (dictionary.length == 0) {
+            dictionary = new DictionaryElement[1];
+            dictionary[0] = new DictionaryElement(1, actualSegment);
+        } else {
+            int index = dictionary[dictionary.length - 1].getIndex();
+            DictionaryElement[] aux = new DictionaryElement[dictionary.length + 1];
+            for (int i = 0; i < dictionary.length; i++) {
+                aux[i] = dictionary[i];
+            }
+            aux[aux.length - 1] = new DictionaryElement(index + 1, actualSegment);
+            dictionary=aux;
+        }
     }
 
 }
