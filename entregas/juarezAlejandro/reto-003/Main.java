4b350f3a9c8f98ff911ed6160947dc7747c22145
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-10-18 22:35:51 +0200

    Empecemos de nuevo

--- entregas/juarezAlejandro/reto-003/Main.java
+++ entregas/juarezAlejandro/reto-003/Main.java
@@ -1,14 +1,8 @@
-public class Main {
-     public static void main(String[] args) {
-        Diccionario diccionario = new Diccionario();
-        Compresor compresor = new Compresor(diccionario);
-
-        String texto = "abababcbdc";
-        String textoComprimido = compresor.imprimir(texto);
-        System.out.println("Secuencia de codigo y caracteres:");
-        compresor.imprimirSecuencia();
-        System.out.println("-".repeat(80));
-        System.out.println("Diccionario:");
-        diccionario.imprimirDiccionario();
-     }
+class Main {
+   public static void main(String[] args) {
+      String texto = "abababcbdc";
+      Compresor compresor = new Compresor();
+      String comprimir = compresor.comprimir(texto);
+      System.out.println(comprimir);
+   }
 }
