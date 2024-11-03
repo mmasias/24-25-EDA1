20d055651c315d7c4dae42330f83b4d3ca9eed42
liamanderson873 <liam.anderson@alumnos.uneatlantico.es>
2024-11-03 15:46:28 +0100

    Primer Commit

---
+++ entregas/reto-004/Spotify.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+class Cancion {
+    private String titulo;
+    private String artista;
+    private int duracion;    
+    private boolean favorita;
+    
+    public Cancion(String titulo, String artista, int duracion) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+        this.favorita = false;
+    }
+
+    public String toString() {
+        return titulo + " - " + artista + " (" + duracion + "s)";
+    }
+
+    public boolean isFavorita() {
+        return favorita;
+    }
+
+    public void setFavorita(boolean favorita) {
+        this.favorita = favorita;
+    }
+}
+
+class Playlist {
+    private String nombre;
+    private NodoCancion cabeza;
+
+    public Playlist(String nombre) {
+        this.nombre = nombre;
+        this.cabeza = null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void agregarCancion(Cancion cancion) {
+        NodoCancion nuevoNodo = new NodoCancion(cancion);
+        nuevoNodo.siguiente = cabeza;
+        cabeza = nuevoNodo;
+    }
+
+    public void mostrarCanciones() {
+        NodoCancion actual = cabeza;
+        int i = 1;
+        while (actual != null) {
+            System.out.println(i + ". " + actual.cancion);
+            actual = actual.siguiente;
+            i++;
+        }
+    }
+}
+
+class NodoCancion {
+    Cancion cancion;
+    NodoCancion siguiente;
+
+    public NodoCancion(Cancion cancion) {
+        this.cancion = cancion;
+        this.siguiente = null;
+    }
+}
