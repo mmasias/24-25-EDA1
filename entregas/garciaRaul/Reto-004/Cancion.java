4c382cd777424684dca7763954f5a31ae341d6ac
Raul <raul.garcia3@alumnos.uneatlantico.es>
2024-11-03 01:47:07 +0100

    Cambio de como he planteado el reto

--- entregas/garciaRaul/Reto-004/Cancion.java
+++ entregas/garciaRaul/Reto-004/Cancion.java
@@ -3,15 +3,17 @@ public class Cancion {
     private String artista;
     private int duracion;    
     private boolean favorita;
-    
+
     public Cancion(String titulo, String artista, int duracion) {
         this.titulo = titulo;
         this.artista = artista;
         this.duracion = duracion;
         this.favorita = false;
     }
+    }
+   
 
     public String toString(){
         return titulo;
+        return titulo + " " + duracion; 
     }
-}
