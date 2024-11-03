0ccb96e3efae7c06d0a01a2e0a7a738bf943c631
Diego <diego.jimnz11@gmail.com>
2024-11-03 19:36:06 +0100

    Creacion del Menu Principal y el manejo de elecciones para el mismo

--- entregas/jimenezdiego/reto-004/reto004/SpotifyApp.java
+++ entregas/jimenezdiego/reto-004/reto004/SpotifyApp.java
@@ -7,4 +7,34 @@ public class SpotifyApp {
     private static ListaCanciones biblioteca = new ListaCanciones();
     private static ColaReproduccion colaReproduccion = new ColaReproduccion();
     private static PilaHistorial historial = new PilaHistorial();
+
+    public static void main(String[] args) {
+        inicializarBiblioteca();
+
+        int opcionPrincipal;
+        do {
+            mostrarBibliotecaInicial();
+            System.out.println("\n=== MENÚ PRINCIPAL ===");
+            System.out.println("1. Reproducción");
+            System.out.println("2. Biblioteca");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcionPrincipal = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (opcionPrincipal) {
+                case 1:
+                    menuReproduccion();
+                    break;
+                case 2:
+                    menuBiblioteca();
+                    break;
+                case 3:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcionPrincipal != 3);
+    }
 }
