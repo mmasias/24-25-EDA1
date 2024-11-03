9193de08e02d24ba54184a554bcf118a65ed8407
kelvia.barros <barroskelvia122@gmail.com>
2024-11-03 16:56:03 +0100

    Se implementa las clases Cancion y Album con sus atributos y métodos.

--- entregas/nevesKelvia/Reto_004/Album.java
+++ entregas/nevesKelvia/Reto_004/Album.java
@@ -1,5 +1,32 @@
 package entregas.nevesKelvia.Reto_004;
 
 public class Album {
-    
+
+    private String titulo;
+    private String artista;
+    private int año;
+
+    public Album(String titulo, String artista, int año) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.año = año;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + artista + " (" + año + ")";
+    }
+
 }
--- entregas/nevesKelvia/Reto_004/Cancion.java
+++ entregas/nevesKelvia/Reto_004/Cancion.java
@@ -1,5 +1,32 @@
 package entregas.nevesKelvia.Reto_004;
 
 public class Cancion {
-    
+    private String titulo;
+    private String artista;
+    private int duracion;
+    private boolean favorita;
+
+    public Cancion(String titulo, String artista, int duracion) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+        this.favorita = false;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setFavorita(boolean favorita) {
+        this.favorita = favorita;
+    }
+
+    public boolean isFavorita() {
+        return favorita;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + artista + " [" + duracion + "s]";
+    }
 }
