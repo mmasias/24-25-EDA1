699e698093afc2a88126aab5fd6c513327938fe3
liamanderson873 <liam.anderson@alumnos.uneatlantico.es>
2024-11-03 15:57:08 +0100

    Clase historial

--- entregas/reto-004/Spotify.java
+++ entregas/reto-004/Spotify.java
@@ -65,3 +65,26 @@ class NodoCancion {
         this.siguiente = null;
     }
 }
+class Historial {
+    private NodoCancion cabeza;
+
+    public Historial() {
+        this.cabeza = null;
+    }
+
+    public void agregarCancion(Cancion cancion) {
+        NodoCancion nuevoNodo = new NodoCancion(cancion);
+        nuevoNodo.siguiente = cabeza;
+        cabeza = nuevoNodo;
+    }
+
+    public void mostrarHistorial() {
+        NodoCancion actual = cabeza;
+        System.out.println("HISTORIAL DE REPRODUCCIÓN:");
+        while (actual != null) {
+            System.out.println(actual.cancion);
+            actual = actual.siguiente;
+        }
+    }
+}
+
