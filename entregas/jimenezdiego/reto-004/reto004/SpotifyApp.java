0249efcb8367a9a0dba8f9e160eae1e668046666
Diego <diego.jimnz11@gmail.com>
2024-11-03 19:28:49 +0100

    Creación de la pagina principal para manejar todas las acciones de la app

---
+++ entregas/jimenezdiego/reto-004/reto004/SpotifyApp.java
@@ -0,0 +1,10 @@
+package reto004;
+
+import java.util.Scanner;
+
+public class SpotifyApp {
+    private static Scanner scanner = new Scanner(System.in);
+    private static ListaCanciones biblioteca = new ListaCanciones();
+    private static ColaReproduccion colaReproduccion = new ColaReproduccion();
+    private static PilaHistorial historial = new PilaHistorial();
+}
