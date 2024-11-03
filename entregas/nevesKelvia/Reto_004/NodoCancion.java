fb41557af4658a7951f1f1f49f5604aebca3a896
kelvia.barros <barroskelvia122@gmail.com>
2024-11-03 17:02:36 +0100

    Agregue una clase NodoCancion para manejar estructura enlazada

---
+++ entregas/nevesKelvia/Reto_004/NodoCancion.java
@@ -0,0 +1,12 @@
+package entregas.nevesKelvia.Reto_004;
+
+public class NodoCancion {
+
+    Cancion cancion;
+    NodoCancion siguiente;
+    NodoCancion anterior;
+
+    public NodoCancion(Cancion cancion) {
+        this.cancion = cancion;
+    }
+}
