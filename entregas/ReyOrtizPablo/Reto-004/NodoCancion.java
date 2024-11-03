06a36cfd24da4110cfdee4a16d1d4c3f3579f050
Pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2024-11-03 19:57:12 +0100

    Creación de clases y trabajando en las clases Reproductor,Playlist, NodoCancion y Cancion

---
+++ entregas/ReyOrtizPablo/Reto-004/NodoCancion.java
@@ -0,0 +1,11 @@
+
+public class NodoCancion {
+    Cancion cancion;
+    NodoCancion siguiente;
+
+    public NodoCancion(Cancion cancion) {
+        this.cancion = cancion;
+        this.siguiente = null;
+    }
+
+}
