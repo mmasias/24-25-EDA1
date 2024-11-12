2a9207bf4954c779f3b66131d988a04d376a2fab
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-11-12 17:31:55 +0100

    Implemento las clases "Cancion" y "Playlist" con atributos para manejar detalles de canciones y playlists.

---
+++ entregas/Morales_JosePablo/Reto-004/Spotify.py
@@ -0,0 +1,14 @@
+class Cancion:
+    def __init__(self, titulo, duracion):
+        self.titulo = titulo
+        self.duracion = duracion
+        self.siguiente = None
+
+    def __str__(self):
+        return f"{self.titulo} ({self.duracion})"
+
+class Playlist:
+    def __init__(self, nombre, artista):
+        self.nombre = nombre
+        self.artista = artista
+        self.canciones = None
