4a7ee3481e608b4c83f1451a9a913b24a4ff6a1c
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-11-13 10:22:41 +0100

    Configuro la instancia de "SistemaReproduccionMusica" e inicializo una playlist predeterminada con una lista de canciones, luego inicio el sistema de reproducción.

--- entregas/Morales_JosePablo/Reto-004/Spotify.py
+++ entregas/Morales_JosePablo/Reto-004/Spotify.py
@@ -282,3 +282,36 @@ class SistemaReproduccionMusica:
                 break
             else:
                 print("Opción inválida. Por favor, elige una opción válida.")
+
+sistema = SistemaReproduccionMusica()
+
+playlist_predeterminada = Playlist("Predeterminada", "Varios")
+playlist_predeterminada.duracion_total = "1 hora, 35 minutos"
+
+playlist_predeterminada.agregar_cancion("Caraluna - Bacilos", "4:26")
+playlist_predeterminada.agregar_cancion("STAY (with Justin Bieber) - The Kid LAROI, Justin Bieber", "2:22")
+playlist_predeterminada.agregar_cancion("It's My Life - Bon Jovi", "3:44")
+playlist_predeterminada.agregar_cancion("Karma Chameleon - Culture Club", "4:12")
+playlist_predeterminada.agregar_cancion("Nunca es Suficiente - Los Angeles Azules, Natalia Lafourcade", "4:26")
+playlist_predeterminada.agregar_cancion("Carry on Wayward Son - Kansas", "5:23")
+playlist_predeterminada.agregar_cancion("Somebody To Love - Queen", "4:56")
+playlist_predeterminada.agregar_cancion("Bam Bam (feat. Ed Sheeran) - Camila Cabello, Ed Sheeran", "3:26")
+playlist_predeterminada.agregar_cancion("THAT'S WHAT I WANT - Lil Nas X", "2:24")
+playlist_predeterminada.agregar_cancion("Yo Viviré - Celia Cruz", "4:31")
+playlist_predeterminada.agregar_cancion("Jump - Van Halen", "4:02")
+playlist_predeterminada.agregar_cancion("Nothing's Gonna Stop Us Now - Starship", "4:30")
+playlist_predeterminada.agregar_cancion("Livin' On a Prayer - Bon Jovi", "4:09")
+playlist_predeterminada.agregar_cancion("Under The Influence - Chris Brown", "3:05")
+playlist_predeterminada.agregar_cancion("Flowers - Miley Cyrus", "3:20")
+playlist_predeterminada.agregar_cancion("CUFF IT - Beyoncé", "3:45")
+playlist_predeterminada.agregar_cancion("I Ain't Worried - OneRepublic", "2:28")
+playlist_predeterminada.agregar_cancion("Live Is Life - Opus", "4:15")
+playlist_predeterminada.agregar_cancion("Another Love - Tom Odell", "4:04")
+playlist_predeterminada.agregar_cancion("Venite Volando - Los Iracundos", "2:36")
+playlist_predeterminada.agregar_cancion("Just Us - James Arthur", "3:35")
+playlist_predeterminada.agregar_cancion("Rasputin - Boney M.", "3:41")
+playlist_predeterminada.agregar_cancion("Born In The U.S.A - Bruce Springsteen", "4:39")
+playlist_predeterminada.agregar_cancion("Eye Of The Tiger - Survivor", "4:04")
+playlist_predeterminada.agregar_cancion("Thank You For The Music - ABBA", "3:49")
+
+sistema.iniciar()
