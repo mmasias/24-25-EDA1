89c2b302c6dd31d13c2d6b1c99fc0e090bb9c695
Martín López de Novales <martin.lopez@alumnos.uneatlantico.es>
2024-11-25 13:41:24 +0100

    menu con switch

--- entregas/lopezMartin/reto_006/Interfaz.java
+++ entregas/lopezMartin/reto_006/Interfaz.java
@@ -17,43 +17,51 @@ public class Interfaz {
 
     public void mostrar(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenidos el editor EDLIN \n Utilice el menu inferior para editar el texto \n ------ \n [L] permite definir la linea activa \n [E]permite editar la linea activa \n [I] permite intercambiar dos lineas \n [B] borra el contenido de la linea activa \n [S] sale del programa \n");
-        String input = sc.nextLine();
-        switch (input.toUpperCase()) {
-            case "L":
-                
-                break;
-
-            case "E":
-
-                break;
-
-            case "I": 
-
-                break;
-
-            case "B":
-
-                break;
-
-            case "D":
-
-                break;
-
-            case "R":
-
-                break;
-
-            case "S":
-
-                break;
-        
-            default:
-                System.out.println("Saliendo del programa");
+        boolean ejecutando = true;
+        System.out.println("Bienvenidos el editor EDLIN");
+        while(ejecutando){
+            System.out.println("Utilice el menu inferior para editar el texto \n ------ \n [L] permite definir la linea activa \n [E]permite editar la linea activa \n [I] permite intercambiar dos lineas \n [B] borra el contenido de la linea activa \n [S] sale del programa \n");
+            String input = sc.nextLine();
+            switch (input.toUpperCase()) {
+                case "L":
+                    
+                    break;
+    
+                case "E":
+    
+                    break;
+    
+                case "I": 
+    
+                    break;
+    
+                case "B":
+    
+                    break;
+    
+                case "D":
+    
+                    break;
+    
+                case "R":
+    
+                    break;
+    
+                case "S":
+                    ejecutando = false;
+                    break;
+            
+                default:
+                    System.out.println("Saliendo del programa");
+            }
         }
-
             
 
+    }
+
+    private void cleanScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
     
 }
