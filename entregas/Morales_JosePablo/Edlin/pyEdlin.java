26a4d4dea86b4120dac7bb40b7e96d08e01e0b72
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-11-25 14:36:29 +0100

    implementación de Ctrl Z, Ctrl Y, Ctrl C y Ctrl V

--- entregas/Morales_JosePablo/Edlin/pyEdlin.java
+++ entregas/Morales_JosePablo/Edlin/pyEdlin.java
@@ -1,6 +1,6 @@
 package pyEdlin;
 
-import java.util.Scanner;
+import java.util.*;
 
 class Edlin {
     public static void main(String[] args) {
@@ -14,13 +14,20 @@ class Edlin {
                 "[I] permite intercambiar dos lineas",
                 "[B] borra el contenido de la linea activa",
                 "[S] sale del programa",
-                "",
+                "[U] deshacer la última acción",
+                "[R] rehacer la última acción deshecha",
+                "[C] copiar la linea activa",
+                "[P] pegar el contenido copiado",
                 ""
         };
 
+        Stack<String[]> undoStack = new Stack<>();
+        Stack<String[]> redoStack = new Stack<>();
+        String clipboard = "";
+
         do {
             print(document, activeLine);
-        } while (processActions(document, activeLine));
+        } while (processActions(document, activeLine, undoStack, redoStack, clipboard));
     }
 
     static void print(String[] document, int[] activeLine) {
@@ -45,8 +52,8 @@ class Edlin {
         System.out.flush();
     }
 
-    static boolean processActions(String[] document, int[] activeLine) {
-        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir");
+    static boolean processActions(String[] document, int[] activeLine, Stack<String[]> undoStack, Stack<String[]> redoStack, String clipboard) {
+        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir | [U]ndo | [R]edo | [C]opiar | [P]egar");
 
         switch (askChar()) {
             case 'S':   case 's':
@@ -55,27 +62,66 @@ class Edlin {
                 setActiveLine(document, activeLine);
                 break;
             case 'E':   case 'e':
+                saveState(document, undoStack);
                 edit(document, activeLine);
                 break;
             case 'I':   case 'i':
+                saveState(document, undoStack);
                 exchangeLines(document);
                 break;
             case 'B':   case 'b':
+                saveState(document, undoStack);
                 delete(document, activeLine);
+                break;
+            case 'U':   case 'u':
+                undo(document, undoStack, redoStack);
+                break;
+            case 'R':   case 'r':
+                redo(document, undoStack, redoStack);
+                break;
+            case 'C':   case 'c':
+                clipboard = copy(document, activeLine);
+                break;
+            case 'P':   case 'p':
+                saveState(document, undoStack);
+                paste(document, activeLine, clipboard);
                 break;
         }
         return true;
     }
 
-    static char askChar() {
-        Scanner input = new Scanner(System.in);
-        return input.next().charAt(0);
+    static void saveState(String[] document, Stack<String[]> stack) {
+        stack.push(document.clone());
+    }
+
+    static void undo(String[] document, Stack<String[]> undoStack, Stack<String[]> redoStack) {
+        if (!undoStack.isEmpty()) {
+            redoStack.push(document.clone());
+            String[] previousState = undoStack.pop();
+            System.arraycopy(previousState, 0, document, 0, document.length);
+        }
+    }
+
+    static void redo(String[] document, Stack<String[]> undoStack, Stack<String[]> redoStack) {
+        if (!redoStack.isEmpty()) {
+            undoStack.push(document.clone());
+            String[] nextState = redoStack.pop();
+            System.arraycopy(nextState, 0, document, 0, document.length);
+        }
+    }
+
+    static String copy(String[] document, int[] activeLine) {
+        return document[activeLine[0]];
+    }
+
+    static void paste(String[] document, int[] activeLine, String clipboard) {
+        document[activeLine[0]] = clipboard;
     }
 
     static void delete(String[] document, int[] activeLine) {
-        System.out.println("Esta acción es irreversible: indique el número de línea activa para confirmarlo ["+activeLine[0]+"]");
-        if (askInt()==activeLine[0]) {
-            document[activeLine[0]]="";
+        System.out.println("Esta acción es irreversible: indique el número de línea activa para confirmarlo [" + activeLine[0] + "]");
+        if (askInt() == activeLine[0]) {
+            document[activeLine[0]] = "";
         }
     }
 
@@ -97,8 +143,8 @@ class Edlin {
         } while (!validLine);
         
         temporaryLine = document[destinationLine];
-        document[destinationLine]=document[originLine];
-        document[originLine]=temporaryLine;
+        document[destinationLine] = document[originLine];
+        document[originLine] = temporaryLine;
     }
 
     static void edit(String[] document, int[] activeLine) {
@@ -123,5 +169,10 @@ class Edlin {
     static int askInt() {
         Scanner input = new Scanner(System.in);
         return input.nextInt();
+    }
+
+    static char askChar() {
+        Scanner input = new Scanner(System.in);
+        return input.next().charAt(0);
     }
 }
