4be37d7c5608c3a2f51165b05a7afb410da14038
Raul <raul.garcia3@alumnos.uneatlantico.es>
2024-11-25 01:36:28 +0100

    Reto casi Terminado

--- entregas/garciaRaul/Reto-005/SistemaControlVersiones.java
+++ entregas/garciaRaul/Reto-005/SistemaControlVersiones.java
@@ -1,3 +1,6 @@
 public class SistemaControlVersiones {
-    
+    public static void main(String[] args) {
+        ArbolVersiones arbolVersiones = new ArbolVersiones();
+        new InterfazUsuario(arbolVersiones).iniciar();
+    }
 }
