4e6f2578830f369ba07f86af47b159a7c6e70c18
micastg <micastg@unirioja.es>
2024-11-28 22:26:38 +0100

    Terminado Deshacer y Rehacer

---
+++ entregas/castanedaMiguel/Reto6/src/ListaNodo.java
@@ -0,0 +1,35 @@
+public class ListaNodo {
+    private Nodo first;
+    private int tamaño=0;
+    public ListaNodo(Nodo newNodo){
+
+    }
+    public ListaNodo(){
+        first=null;
+    }
+
+    public void insertarPrimerNodo(Nodo newNodo){
+        newNodo.setSiguiente(first);
+        first=newNodo;
+        Nodo actual=first;
+        if(tamaño<5){
+            tamaño++;
+        }else{
+            for(int i=0;i<4;i++){
+                actual=actual.siguiente;
+            }
+            actual.siguiente=null;
+        }
+    }
+
+    public Nodo getNodo(){
+        return first;
+    }
+
+    public void quitarPrimerNodo(){
+        first=first.siguiente; tamaño--;
+    }
+
+    public void clear(){first=null;}
+
+}
