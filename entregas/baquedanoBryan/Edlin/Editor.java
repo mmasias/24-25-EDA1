f6885cb264bc65c8e8b16f753d84beedde99a751
enmabry <bryanbaquedano11@gmail.com>
2024-11-29 00:23:21 +0100

    Desarrollado Métodos
    He decidido que el Edlin como tal, no tiene la responsabilidad de tener los métodos, o de tener el "motor" ahí, entonces lo que he hecho, es sacar sus métodos, adaptarlos a mi clase Editor, también tomando en cuenta la nueva clase pila.

--- entregas/baquedanoBryan/Edlin/Editor.java
+++ entregas/baquedanoBryan/Edlin/Editor.java
@@ -5,6 +5,10 @@ public class Editor {
     private String portapapeles;
     private int lineaActiva;
 
+    private boolean lineaValida(int linea) {
+        return linea >= 0 && linea < document.length;
+    }
+
     public Editor(int tamanioDocumento) {
         this.document = new String[tamanioDocumento];
         for (int i = 0; i < tamanioDocumento; i++) {
@@ -16,6 +20,50 @@ public class Editor {
         this.lineaActiva = 0;
     }
 
-    
-    
+    public void editar(int linea, String nuevoContenido) {
+        if (!lineaValida(linea)) {
+            System.out.println("Línea no válida.");
+            return;
+        }
+        historialDeshacer.push(document);
+        historialRehacer = new Pila();
+        document[linea] = nuevoContenido;
+    }
+
+    public void borrar(int linea) {
+        if (!lineaValida(linea)) {
+            System.out.println("Línea no válida.");
+            return;
+        }
+        historialDeshacer.push(document);
+        historialRehacer = new Pila();
+        document[linea] = "";
+    }
+
+    public void intercambiar(int linea1, int linea2) {
+        if (!lineaValida(linea1) || !lineaValida(linea2)) {
+            System.out.println("Una o ambas líneas no son válidas.");
+            return;
+        }
+        historialDeshacer.push(document);
+        historialRehacer = new Pila();
+        String temp = document[linea1];
+        document[linea1] = document[linea2];
+        document[linea2] = temp;
+    }
+
+
+    public void setLineaActiva(int nuevaLineaActiva) {
+        if (!lineaValida(nuevaLineaActiva)) {
+            System.out.println("Línea no válida.");
+            return;
+        }
+        this.lineaActiva = nuevaLineaActiva;
+    }
+
+    public void mostrarDocumento() {
+        for (int i = 0; i < document.length; i++) {
+            System.out.println((i == lineaActiva ? "* " : "  ") + i + ": " + document[i]);
+        }
+    }    
 }
