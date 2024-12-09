bc1eeafa12c9757ab2d00563497219e52ed41b3d
marcosgutierrez6 <marcos.gutierrez1@alumnos.uneatlantico.es>
2024-12-09 12:18:55 +0100

    Codigo extra - Reto-006

--- entregas/gutierrezMarcos/Reto-006/DocumentEditor.java
+++ entregas/gutierrezMarcos/Reto-006/DocumentEditor.java
@@ -5,20 +5,24 @@ public class DocumentEditor {
     private History history;
     private Scanner input;
     private String clipboard;
+    private FileManager fileManager;
 
     public DocumentEditor() {
         this.document = new Document(new String[]{
                 "Bienvenidos al editor EDLIN",
-                "Utilice el menu inferior para editar el texto",
+                "Utilice el menú inferior para editar el texto",
                 "------",
-                "[L] permite definir la linea activa",
-                "[E] permite editar la linea activa",
-                "[I] permite intercambiar dos lineas",
-                "[B] borra el contenido de la linea activa",
+                "[L] permite definir la línea activa",
+                "[E] permite editar la línea activa",
+                "[I] permite intercambiar dos líneas",
+                "[B] borra el contenido de la línea activa",
                 "[C] copia el contenido de la línea activa",
                 "[P] pega el contenido copiado en la línea activa",
                 "[Z] deshacer la última acción",
                 "[Y] rehacer la última acción deshecha",
+                "[G] guarda el documento en archivo",
+                "[R] carga el documento desde archivo",
+                "[D] elimina una línea del archivo",
                 "[S] sale del programa",
                 "",
                 ""
@@ -26,6 +30,7 @@ public class DocumentEditor {
         this.history = new History(document);
         this.input = new Scanner(System.in);
         this.clipboard = "";
+        this.fileManager = new FileManager("documento.txt");
     }
 
     public void run() {
@@ -35,7 +40,7 @@ public class DocumentEditor {
     }
 
     private boolean processActions() {
-        System.out.println("Comandos: [L]inea activa | [E]ditar | [I]ntercambiar | [B]orrar | [C]Copiar | [P]Pegar | [Z]Deshacer | [Y]Rehacer | [S]alir");
+        System.out.println("Comandos: [L]ínea activa | [E]ditar | [I]ntercambiar | [B]orrar | [C]Copiar | [P]Pegar | [Z]Deshacer | [Y]Rehacer | [G]uardar | [R]ecuperar | [D]elete línea | [S]alir");
         char action = askChar();
 
         switch (Character.toLowerCase(action)) {
@@ -77,8 +82,46 @@ public class DocumentEditor {
             case 'y':
                 history.redo(document);
                 break;
+            case 'g':
+                guardarDocumentoEnArchivo();
+                break;
+            case 'r':
+                cargarDocumentoDesdeArchivo();
+                break;
+            case 'd':
+                eliminarLineaArchivo();
+                break;
         }
         return true;
+    }
+
+    private void guardarDocumentoEnArchivo() {
+        String contenido = String.join("\n", document.getLines());
+        if (fileManager.createFile(contenido)) {
+            System.out.println("Documento guardado exitosamente en 'documento.txt'.");
+        } else {
+            System.err.println("Error al guardar el documento.");
+        }
+    }
+
+    private void cargarDocumentoDesdeArchivo() {
+        String contenido = fileManager.readFile();
+        if (contenido != null) {
+            String[] lineas = contenido.split(System.lineSeparator());
+            document.setLines(lineas);
+            System.out.println("Documento cargado exitosamente desde 'documento.txt'.");
+        } else {
+            System.err.println("Error al cargar el documento.");
+        }
+    }
+
+    private void eliminarLineaArchivo() {
+        int linea = askInt("Indique el número de línea a eliminar del archivo: ");
+        if (fileManager.deleteLine(linea)) {
+            System.out.println("Línea eliminada exitosamente del archivo.");
+        } else {
+            System.err.println("Error al eliminar la línea del archivo.");
+        }
     }
 
     private char askChar() {
