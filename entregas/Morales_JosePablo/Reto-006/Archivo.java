09e053cacb618b615f8f6aed30fb7f7eee8d9b8f
JP <144849667+JPMoralesl004@users.noreply.github.com>
2024-12-10 10:29:09 +0100

    Agrego la clase Archivo para gestionar archivos con nombre, contenido y fechas, e historial de versiones.

---
+++ entregas/Morales_JosePablo/Reto-006/Archivo.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.util.List;
+
+public class Archivo {
+    public String nombre;
+    public String contenido;
+    public Date fechaCreacion;
+    public Date fechaModificacion;
+
+    public List<VersionArchivo> obtenerHistorial() {
+        return null;
+    }
+}
